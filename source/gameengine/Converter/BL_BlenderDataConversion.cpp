af5b133526cb44352bef5c59f93730f2b2a5eec5
Porteries Tristan <republicthunderbolt9@gmail.com>
2016-06-06 12:31:40 +0000

    Rename lodList to lodManager in BL_BlenderDataConversion.cpp.

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -951,15 +951,15 @@ static void BL_CreatePhysicsObjectNew(KX_GameObject* gameobj,
 	}
 }
 
-static KX_LodManager *lodlist_from_blenderobject(Object *ob, KX_Scene *scene, KX_BlenderSceneConverter *converter, bool libloading)
+static KX_LodManager *lodmanager_from_blenderobject(Object *ob, KX_Scene *scene, KX_BlenderSceneConverter *converter, bool libloading)
 {
-	KX_LodManager *lodList = new KX_LodManager(ob, scene, converter, libloading);
-	if (lodList->Empty()) {
-		lodList->Release();
+	KX_LodManager *lodManager = new KX_LodManager(ob, scene, converter, libloading);
+	if (lodManager->Empty()) {
+		lodManager->Release();
 		return NULL;
 	}
 
-	return lodList;
+	return lodManager;
 }
 
 static KX_LightObject *gamelight_from_blamp(Object *ob, Lamp *la, unsigned int layerflag, KX_Scene *kxscene, RAS_IRasterizer *rasterizer, KX_BlenderSceneConverter *converter)
@@ -1084,7 +1084,7 @@ static KX_GameObject *gameobject_from_blenderobject(
 		gameobj->AddMesh(meshobj);
 
 		// gather levels of detail
-		KX_LodManager *lodManager = lodlist_from_blenderobject(ob, kxscene, converter, libloading);
+		KX_LodManager *lodManager = lodmanager_from_blenderobject(ob, kxscene, converter, libloading);
 		gameobj->SetLodManager(lodManager);
 
 		// for all objects: check whether they want to
