2e81ce5310cfb730cae0e9ed4180f4e7da142b54
Porteries Tristan <republicthunderbolt9@gmail.com>
2016-08-27 15:14:48 +0000

    FIx ignore layer inverted.

--- source/gameengine/Ketsji/KX_CubeMapManager.cpp
+++ source/gameengine/Ketsji/KX_CubeMapManager.cpp
@@ -110,7 +110,7 @@ void KX_CubeMapManager::RenderCubeMap(RAS_IRasterizer *rasty, KX_CubeMap *cubeMa
 		MT_Transform trans(m_camera->GetWorldToCamera());
 		MT_Matrix4x4 viewmat(trans);
 		m_camera->SetModelviewMatrix(viewmat);
-		m_scene->CalculateVisibleMeshes(rasty, m_camera, cubeMap->GetIgnoreLayers());
+		m_scene->CalculateVisibleMeshes(rasty, m_camera, ~cubeMap->GetIgnoreLayers());
 
 		/* Update animations to use the culling of each faces, BL_ActionManager avoid redundants
 		 * updates internally. */
