c1d673c46a59d6a7458513fa32d3c8697d8ee75e
Porteries Tristan <republicthunderbolt9@gmail.com>
2016-11-24 17:41:53 +0000

    UPBGE: Fix environment lighting in blenderplayer.
    Similary to 88314cb2a0c3f8b18cf7b486bd7e42c8edc809c9, the horizon,
    zenith, exposure and range settings were not updated before compiling
    the materials.
    
    A call to KX_WorldInfo::UpdateBackGround is added during the conversion.

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -1413,6 +1413,7 @@ void BL_ConvertBlenderObjects(struct Main* maggie,
 	// convert world
 	KX_WorldInfo* worldinfo = new KX_WorldInfo(blenderscene, blenderscene->world);
 	worldinfo->UpdateWorldSettings(rendertools);
+	worldinfo->UpdateBackGround(rendertools);
 	kxscene->SetWorldInfo(worldinfo);
 
 	int activeLayerBitInfo = blenderscene->lay;
