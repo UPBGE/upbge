38ad9cc06c1e678948e3fb7610c1a7c3e6e33574
Porteries Tristan <republicthunderbolt9@gmail.com>
2016-12-19 12:04:20 +0000

    UPBGE: Avoid using text full length.
    Previously when a text was rendered with BLF_draw the text size passed
    was the maximal text size: 65535. But it's useless to pass the maximal
    size as we can get the size of the string.

--- source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
+++ source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
@@ -2173,7 +2173,7 @@ void RAS_OpenGLRasterizer::RenderText3D(
 
 	BLF_size(fontid, size, dpi);
 	BLF_position(fontid, 0, 0, 0);
-	BLF_draw(fontid, text.c_str(), 65535);
+	BLF_draw(fontid, text.c_str(), text.size());
 
 	BLF_disable(fontid, BLF_MATRIX | BLF_ASPECT);
 
@@ -2208,14 +2208,14 @@ void RAS_OpenGLRasterizer::RenderText2D(
 		glColor3ub(0, 0, 0);
 		BLF_size(blf_mono_font, 11, 72);
 		BLF_position(blf_mono_font, (float)xco + 1, (float)(height - yco - 1), 0.0f);
-		BLF_draw(blf_mono_font, text.c_str(), 65535); /* XXX, use real len */
+		BLF_draw(blf_mono_font, text.c_str(), text.size());
 	}
 
 	/* the actual drawing */
 	glColor3ub(255, 255, 255);
 	BLF_size(blf_mono_font, 11, 72);
 	BLF_position(blf_mono_font, (float)xco, (float)(height - yco), 0.0f);
-	BLF_draw(blf_mono_font, text.c_str(), 65535); /* XXX, use real len */
+	BLF_draw(blf_mono_font, text.c_str(), text.size());
 
 	SetMatrixMode(RAS_PROJECTION);
 	PopMatrix();
