2c0c7c1e7344b8fd5b9bc756267fb6a366820392
Porteries Tristan <republicthunderbolt9@gmail.com>
2017-01-21 16:54:05 +0000

    Fix furstum volume size.

--- source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
+++ source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
@@ -898,11 +898,11 @@ void RAS_OpenGLRasterizer::DrawDebugCameraFrustum(SCA_IScene *scene, const MT_Ma
 {
 	MT_Vector3 box[8];
 
-	box[0][0] = box[1][0] = box[4][0] = box[5][0] = -0.5f;
-	box[2][0] = box[3][0] = box[6][0] = box[7][0] = 0.5f;
-	box[0][1] = box[3][1] = box[4][1] = box[7][1] = -0.5f;
-	box[1][1] = box[2][1] = box[5][1] = box[6][1] = 0.5f;
-	box[0][2] = box[1][2] = box[2][2] = box[3][2] = 0.1f;
+	box[0][0] = box[1][0] = box[4][0] = box[5][0] = -1.0f;
+	box[2][0] = box[3][0] = box[6][0] = box[7][0] = 1.0f;
+	box[0][1] = box[3][1] = box[4][1] = box[7][1] = -1.0f;
+	box[1][1] = box[2][1] = box[5][1] = box[6][1] = 1.0f;
+	box[0][2] = box[1][2] = box[2][2] = box[3][2] = 0.0f;
 	box[4][2] = box[5][2] = box[6][2] = box[7][2] = 1.0f;
 
 	MT_Matrix4x4 mv = viewmat.inverse() * projmat.inverse();
