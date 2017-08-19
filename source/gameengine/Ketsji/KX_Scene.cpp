7cfbd97bb851734cde0640af6b1e2a6c38839421
Porteries Tristan <republicthunderbolt9@gmail.com>
2017-08-19 11:05:59 +0200

    UPBGE: Fix object with timer deleted before timer end.
    Previously the object in m_tempObjectList were removed only
    when the timer ended, but if the user ask to delete the object
    before the timer does, then the object still remains in
    m_tempObjectList and at the next logic frame we will intent
    to read the timer property from all the object in the list
    and we will get invalid pointers.
    
    To solve this issue the object is removed from the temporary
    list only in NewRemoveObject.
    
    Fix issue: #565.

--- source/gameengine/Ketsji/KX_Scene.cpp
+++ source/gameengine/Ketsji/KX_Scene.cpp
@@ -1203,6 +1203,11 @@ bool KX_Scene::NewRemoveObject(KX_GameObject *gameobj)
     m_euthanasyobjects.erase(euthit);
   }
 
+  const std::vector<KX_GameObject *>::const_iterator tempit = std::find(m_tempObjectList.begin(), m_tempObjectList.end(), gameobj);
+  if (tempit != m_tempObjectList.end()) {
+      m_tempObjectList.erase(tempit);
+  }
+
   if (gameobj == m_active_camera) {
     // no AddRef done on m_active_camera so no Release
     // m_active_camera->Release();
@@ -1310,22 +1315,18 @@ void KX_Scene::RenderDebugProperties(RAS_DebugDraw &debugDraw,
 void KX_Scene::LogicBeginFrame(double curtime, double framestep)
 {
   // have a look at temp objects ...
-  for (std::vector<KX_GameObject *>::iterator it = m_tempObjectList.begin();
-       it != m_tempObjectList.end();) {
-    KX_GameObject *gameobj = *it;
+  for (KX_GameObject *gameobj : m_tempObjectList) {
     CFloatValue *propval = (CFloatValue *)gameobj->GetProperty("::timebomb");
 
     if (propval) {
-      float timeleft = propval->GetNumber() - framestep;
+      const float timeleft = propval->GetNumber() - framestep;
 
       if (timeleft > 0) {
         propval->SetFloat(timeleft);
-        ++it;
       }
       else {
-        // remove obj
+        // remove obj, remove the object from tempObjectList in NewRemoveObject only.
         DelayedRemoveObject(gameobj);
-        it = m_tempObjectList.erase(it);
       }
     }
     else {
