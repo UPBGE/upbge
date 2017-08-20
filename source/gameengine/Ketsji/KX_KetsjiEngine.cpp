a56326951c89dd34b39a15a4946379abeb277eef
Porteries Tristan <republicthunderbolt9@gmail.com>
2017-08-20 23:59:28 +0200

    Set render queries computation time spent in overhead category.

--- source/gameengine/Ketsji/KX_KetsjiEngine.cpp
+++ source/gameengine/Ketsji/KX_KetsjiEngine.cpp
@@ -253,9 +253,13 @@ void KX_KetsjiEngine::StartEngine()
 
 void KX_KetsjiEngine::BeginFrame()
 {
+	m_logger.StartLog(tc_overhead, m_kxsystem->GetTimeInSeconds());
+
 	for (RAS_Query& query : m_renderQueries) {
 		query.Begin();
 	}
+
+	m_logger.StartLog(tc_rasterizer, m_kxsystem->GetTimeInSeconds());
 
 	m_rasterizer->BeginFrame(m_frameTime);
 
@@ -266,12 +270,13 @@ void KX_KetsjiEngine::EndFrame()
 {
 	m_rasterizer->MotionBlur();
 
+	// Show profiling info
+	m_logger.StartLog(tc_overhead, m_kxsystem->GetTimeInSeconds());
+
 	for (RAS_Query& query : m_renderQueries) {
 		query.End();
 	}
 
-	// Show profiling info
-	m_logger.StartLog(tc_overhead, m_kxsystem->GetTimeInSeconds());
 	if (m_flags & (SHOW_PROFILE | SHOW_FRAMERATE | SHOW_DEBUG_PROPERTIES | SHOW_RENDER_QUERIES)) {
 		RenderDebugProperties();
 	}
