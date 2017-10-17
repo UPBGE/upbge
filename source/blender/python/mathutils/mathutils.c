48fe709acf1e9099902bc4b0da77b9f187b45c90
Ulysse Martin <youle31@users.noreply.github.com>
2017-10-17 12:21:44 +0200

    fix tot number of mathutils callbacks

--- source/blender/python/mathutils/mathutils.c
+++ source/blender/python/mathutils/mathutils.c
@@ -429,7 +429,7 @@ int mathutils_deepcopy_args_check(PyObject *args)
 /* Mathutils Callbacks */
 
 /* for mathutils internal use only, eventually should re-alloc but to start with we only have a few users */
-#define MATHUTILS_TOT_CB 17
+#define MATHUTILS_TOT_CB 19
 static Mathutils_Callback *mathutils_callbacks[MATHUTILS_TOT_CB] = {NULL};
 
 unsigned char Mathutils_RegisterCallback(Mathutils_Callback *cb)
