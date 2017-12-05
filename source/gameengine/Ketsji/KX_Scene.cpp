62fd4cf5426ae6840e809cea6d4ae0a8feb2e76c
Ulysse Martin <youle31@users.noreply.github.com>
2017-12-05 21:58:12 +0100

    temp fix for volumes flicker (not planar flicker)
    needs more tests

--- source/gameengine/Ketsji/KX_Scene.cpp
+++ source/gameengine/Ketsji/KX_Scene.cpp
@@ -1933,7 +1933,9 @@ void KX_Scene::EeveePostProcessingHackBegin(const KX_CullingNodeList& nodes)
 			if (view_is_valid) {
 				effects->taa_current_sample += 1;
 
-				effects->taa_alpha = max_ff(0.1, 1.0f / (float)(effects->taa_current_sample));
+				if (effects->taa_current_sample < 50) {
+					effects->taa_alpha = 1.0f / (float)(effects->taa_current_sample);
+				}
 
 				double ht_point[2];
 				double ht_offset[2] = { 0.0, 0.0 };
