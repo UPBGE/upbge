1382619ce630f0a46b210de1f658e8c99466e477
Ulysse Martin <youle31@users.noreply.github.com>
2018-05-06 11:42:52 +0200

    Revert "Remove game properties in image editor" here seems right
    This reverts commit fb24813d64dd8fa2ddb8c0e55004d3792281c8d7.

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -3929,6 +3929,8 @@ static void direct_link_image(FileData *fd, Image *ima)
 		}
 		ima->rr = NULL;
 	}
+
+	ima->repbind = NULL;
 	
 	/* undo system, try to restore render buffers */
 	if (fd->imamap) {
