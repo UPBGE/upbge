2f7763f53cecb656a2a11bf0493a506828e31694
Porteries Tristan <republicthunderbolt9@gmail.com>
2018-06-25 11:24:48 +0200

    example boolean

--- release/scripts/startup/logicnode_builtins.py
+++ release/scripts/startup/logicnode_builtins.py
@@ -9,9 +9,12 @@ class LogicNode(bpy.types.Node):
     bl_outputs = {}
     bl_inputs = {}
 
+    def add_trigger_in(self, name):
+        self.inputs.new("NodeSocketLogic", name)
+
     def init(self, context):
-        if self.bl_trigger_in is not "":
-            self.inputs.new("NodeSocketLogic", self.bl_trigger_in)
+        if self.bl_trigger_in is not None:
+            self.add_trigger_in(self.bl_trigger_in)
 
         for o in self.bl_triggers_out:
             self.outputs.new("NodeSocketLogic", o)
@@ -21,6 +24,23 @@ class LogicNode(bpy.types.Node):
 
         for name, socket in self.bl_outputs.items():
             self.outputs.new(socket, name)
+
+class LogicNodeBoolean(LogicNode):
+    bl_idname = "LogicNodeBoolean"
+    bl_label = "Logic Boolean"
+
+    def unlinked_triggers(self):
+        return [input for input in self.inputs if input.bl_idname == "NodeSocketLogic" and not input.is_linked]
+
+    def update(self):
+        unlinked = self.unlinked_triggers()
+        for input in unlinked[1:]:
+            self.inputs.remove(input)
+
+    def insert_link(self, link):
+        unlinked = self.unlinked_triggers()
+        if len(unlinked) == 1:
+            self.add_trigger_in("Trigger In")
 
 class LogicNodeBasicMotion(LogicNode):
     bl_idname = "LogicNodeBasicMotion"
@@ -38,12 +58,16 @@ class LogicNodeCategory(SortedNodeCategory):
         return (context.space_data.tree_type == "LogicNodeTree")
 
 logic_node_categories = [
-    LogicNodeCategory("MOTION", "Basic Motion", items=[
+    LogicNodeCategory("BOOLEAN", "Boolean", items=[
+        NodeItem("LogicNodeBoolean"),
+        ]),
+    LogicNodeCategory("MOTION", "Motion", items=[
         NodeItem("LogicNodeBasicMotion"),
         ])
     ]
 
 def register():
+    bpy.utils.register_class(LogicNodeBoolean)
     bpy.utils.register_class(LogicNodeBasicMotion)
 
     nodeitems_utils.register_node_categories('LOGIC NODES', logic_node_categories)
