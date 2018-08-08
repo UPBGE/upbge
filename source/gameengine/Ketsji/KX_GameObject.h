229ff4ef053bff116654233cf1aad4bc99a5312e
tristan <tristan@upbge.org>
2018-08-08 21:52:05 +0200

    UPBGE: Fix orthographic camera getScreenRay.
    Previously the function getScreenRay was adapted to only perspective
    cameras because the ray cast was always starting from the center of
    the camera.
    In case of orthographic the ray cast should start from a point unprojected
    from the near plane and continue to in the direction of the camera
    (-Z orientation axis).
    This is proceeded in getScreenRay and the indirect call to getScreenVect
    is removed.
    
    Fix issue #765.

--- source/gameengine/Ketsji/KX_GameObject.h
+++ source/gameengine/Ketsji/KX_GameObject.h
@@ -147,7 +147,15 @@ public:
 	 * This structure is created during ray cast and passed as argument 
 	 * "data" to functions KX_GameObject::NeedRayCast and KX_GameObject::RayHit.
 	 */
-	struct RayCastData;
+	struct RayCastData
+	{
+		RayCastData(const std::string& prop, bool xray, unsigned int mask);
+
+		std::string m_prop;
+		bool m_xray;
+		unsigned int m_mask;
+		KX_GameObject *m_hitObject;
+	};
 
 	/**
 	 * Helper function for modules that can't include KX_ClientObjectInfo.h
