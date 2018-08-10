blender/blender

commit 1b74ec9564cb6edf83f5fd8811c67f019fc9f25c

fix for too small MATHUTILS_TOT_CB when the game engine runs.

source/blender/python/mathutils/mathutils.c
@@ -292,7 +292,7 @@ int mathutils_deepcopy_args_check(PyObject *args)
 /* Mathutils Callbacks */
 
 /* for mathutils internal use only, eventually should re-alloc but to start with we only have a few users */
-#define MATHUTILS_TOT_CB 8
+#define MATHUTILS_TOT_CB 10
 static Mathutils_Callback *mathutils_callbacks[MATHUTILS_TOT_CB] = {NULL};
 
 unsigned char Mathutils_RegisterCallback(Mathutils_Callback *cb)
@@ -305,7 +305,7 @@ unsigned char Mathutils_RegisterCallback(Mathutils_Callback *cb)
 			return i;
 	}
 
-	BLI_assert(i < MATHUTILS_TOT_CB);
+	BLI_assert(i + 1 < MATHUTILS_TOT_CB);
 
 	mathutils_callbacks[i] = cb;
 	return i;

UPBGE/upbge

commit f30d10d2a004c8261a0d573db69c99d908a81b8e

UPBGE: Fix mathutils callbacks registration.
The assertion in Mathutils_RegisterCallback was raised when all the callbacks
were registered (freestyle and bmesh too).
But this assertion introduced in 1b74ec9564cb is wrong. The test i + 1 < MAX
suppose that the last callback is a null sentinel. But this is not the
case as no one is using this to stop a loop and the loop in
Mathutils_RegisterCallback must stop when it find a null slot or that the
slot is already containing the callback, otherwise it's an error.
It's impossible to stop the loop with all regular slots registered and
and different from the one intenting to register.

Fix a part of issue #785.

source/blender/python/mathutils/mathutils.c
@@ -442,7 +442,7 @@ unsigned char Mathutils_RegisterCallback(Mathutils_Callback *cb)
 			return i;
 	}
 
-	BLI_assert(i + 1 < MATHUTILS_TOT_CB);
+	BLI_assert(i < MATHUTILS_TOT_CB);
 
 	mathutils_callbacks[i] = cb;
 	return i;