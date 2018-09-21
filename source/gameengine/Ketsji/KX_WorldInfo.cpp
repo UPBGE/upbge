1ae2bb866b63201094560c444cceb2ed89521e2a
tristan <tristan@upbge.org>
2018-09-21 12:37:17 +0200

    UPBGE: Fix planar world background render.
    Previously the world background render was disabling the culling face
    to render te background triangle without caring about face settings.
    This was changed into setting front face to CCW.
    But the side effect of using front face setting, RAS_Rasterizer::SetFrontFace,
    was that this function is inversing the face if m_invertFrontFace or
    m_camnegscale is true and this was the case for m_invertFrontFace when
    rendering a plane texture.
    
    Practically, the front face was CW and the world background wasn't rendered.
    
    To fix this issue SetFrontFace is replaced back to SetCullFace and a
    comment is left.

--- source/gameengine/Ketsji/KX_WorldInfo.cpp
+++ source/gameengine/Ketsji/KX_WorldInfo.cpp
@@ -203,7 +203,10 @@ void KX_WorldInfo::RenderBackground(RAS_Rasterizer *rasty)
 			GPU_material_bind(m_gpuMat, m_scene->lay, 1.0f, false, rasty->GetViewMatrix().Data(),
 			                  rasty->GetViewInvMatrix().Data(), texcofac, false);
 
-			rasty->SetFrontFace(true);
+			/* Disable cull face instead of setting front face as it could be
+			 * inversed in planar rendering.
+			 */
+			rasty->SetCullFace(false);
 			rasty->Enable(RAS_Rasterizer::RAS_DEPTH_TEST);
 			rasty->SetDepthFunc(RAS_Rasterizer::RAS_ALWAYS);
 
