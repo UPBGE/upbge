f644552e417a6e1a880adc97ae44e82104a8ac66
Porteries Tristan <republicthunderbolt9@gmail.com>
2018-09-30 15:34:15 +0200

    UPBGE: Fix and simplify frame time management.
    Previously a commit introduce for external clock the targeting of one frame
    at every call to NextFrame, not more.
    The original behaviour is if the user enabled fixed framerate then compute
    as many as frame possible respecting the FPS value (m_ticrate).
    This behaviour is restored in the refactor.
    
    The refactor is simplyfing the time calculation and isolating it.
    The function KX_KetsjiEngine::GetFrameTimes is returning a struct FrameTimes
    with the number of frames to proceed and the time/frame step.
    Inside this function we first update the clock time if the user isn't (case
    of external clock) then the elapsed time is computed. Next the timestep and
    frame number are computed using fixed value or elapsed time. The number of frame
    is clamped by max logic/physics frame using the simple method: If the
    number of frame is higher than the maximum for logic or physics, then clamp
    it and set timestep to the elasped time (which is always greater than fixed
    time step 1/FPS) divided by the maximum number of frames.
    Finally if the user is not using fixed framerate update the previous clock
    else if the number of frames to proceed is zero sleep.
    Issue were noticed using a sleep, they were caused by the resolution of the
    sleep which is about some ~10us, to fix this a part of the sleep is consumed
    with std::this_thread::sleep_for and the rest is busy wait.
    
    CM_Clock has now a function Reset to ensure it starts from 0, this is useful
    for external clock starting at 0.

--- source/gameengine/Common/CM_Clock.cpp
+++ source/gameengine/Common/CM_Clock.cpp
@@ -1,5 +1,15 @@
 #include "CM_Clock.h"
 
+CM_Clock::CM_Clock()
+{
+	Reset();
+}
+
+void CM_Clock::Reset()
+{
+	m_start = m_clock.now();
+}
+
 double CM_Clock::GetTimeSecond() const
 {
 	return GetTimeNano() * 1e-9;
@@ -8,5 +18,5 @@ double CM_Clock::GetTimeSecond() const
 CM_Clock::Rep CM_Clock::GetTimeNano() const
 {
 	const std::chrono::high_resolution_clock::time_point now = m_clock.now();
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+	return std::chrono::duration_cast<std::chrono::nanoseconds>(now - m_start).count();
 }
