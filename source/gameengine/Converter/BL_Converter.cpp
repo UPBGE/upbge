wardPlaced/blender

UPBGE: Implement resource collection.
Previously to access to a resource (mesh, action) or getting a converted
object by name, both place where used : BL_Converter and SCA_LogicManager.

To avoid managing two classes BL_ResourceCollection is introduced. Its
goal is to own all the material, mesh and action resource and track a name
map to mesh, action and game object from conversion stage.

This class is owned by KX_Scene and instanciated by moving content of a
BL_SceneConverter (that is still needed because of the map to blender object
is maintain during conversion).

source/gameengine/Converter/BL_Converter.cpp
@@ -35,9 +35,7 @@
 
 #include "KX_Scene.h"
 #include "KX_GameObject.h"
-#include "KX_WorldInfo.h"
 #include "KX_Mesh.h"
-#include "RAS_BucketManager.h"
 #include "KX_PhysicsEngineEnums.h"
 #include "KX_KetsjiEngine.h"
 #include "KX_PythonInit.h" // So we can handle adding new text datablocks for Python to import
@@ -51,17 +49,13 @@
 
 #include "LA_SystemCommandLine.h"
 
+#include "RAS_MaterialBucket.h"
+
 #include "DummyPhysicsEnvironment.h"
 
 #ifdef WITH_BULLET
 #  include "CcdPhysicsEnvironment.h"
 #endif
-
-#include "EXP_PropString.h"
-
-#ifdef WITH_PYTHON
-#  include "Texture.h" // For FreeAllTextures.
-#endif  // WITH_PYTHON
 
 // This list includes only data type definitions
 #include "DNA_scene_types.h"
@@ -70,6 +64,7 @@
 extern "C" {
 #  include "DNA_mesh_types.h"
 #  include "DNA_material_types.h"
+#  include "DNA_action_types.h"
 #  include "BLI_blenlib.h"
 #  include "BLI_linklist.h"
 #  include "BLO_readfile.h"
@@ -85,45 +80,6 @@ extern "C" {
 #include "CM_Message.h"
 
 #include <cstring>
-
-BL_Converter::SceneSlot::SceneSlot() = default;
-
-BL_Converter::SceneSlot::SceneSlot(const BL_SceneConverter& converter)
-{
-	Merge(converter);
-}
-
-BL_Converter::SceneSlot::~SceneSlot() = default;
-
-void BL_Converter::SceneSlot::Merge(BL_Converter::SceneSlot& other)
-{
-	m_interpolators.insert(m_interpolators.begin(),
-	                       std::make_move_iterator(other.m_interpolators.begin()),
-	                       std::make_move_iterator(other.m_interpolators.end()));
-	m_materials.insert(m_materials.begin(),
-	                   std::make_move_iterator(other.m_materials.begin()),
-	                   std::make_move_iterator(other.m_materials.end()));
-	m_meshobjects.insert(m_meshobjects.begin(),
-	                     std::make_move_iterator(other.m_meshobjects.begin()),
-	                     std::make_move_iterator(other.m_meshobjects.end()));
-	m_objectInfos.insert(m_objectInfos.begin(),
-	                     std::make_move_iterator(other.m_objectInfos.begin()),
-	                     std::make_move_iterator(other.m_objectInfos.end()));
-	m_actionToInterp.insert(other.m_actionToInterp.begin(), other.m_actionToInterp.end());
-}
-
-void BL_Converter::SceneSlot::Merge(const BL_SceneConverter& converter)
-{
-	for (KX_BlenderMaterial *mat : converter.m_materials) {
-		m_materials.emplace_back(mat);
-	}
-	for (KX_Mesh *meshobj : converter.m_meshobjects) {
-		m_meshobjects.emplace_back(meshobj);
-	}
-	for (BL_ConvertObjectInfo *info : converter.m_objectInfos) {
-		m_objectInfos.emplace_back(info);
-	}
-}
 
 BL_Converter::BL_Converter(Main *maggie, KX_KetsjiEngine *engine)
 	:m_maggie(maggie),
@@ -231,65 +187,12 @@ void BL_Converter::ConvertScene(BL_SceneConverter& converter, bool libloading)
 		m_alwaysUseExpandFraming,
 		libloading);
 
-	m_sceneSlots.emplace(scene, converter);
-}
-
-void BL_Converter::FinalizeSceneData(const BL_SceneConverter& converter, KX_Scene *mergeScene)
-{
-	for (KX_Mesh *mesh : converter.m_meshobjects) {
-		mesh->ReplaceScene(mergeScene);
-	}
-
-	for (KX_BlenderMaterial *mat : converter.m_materials) {
-		// Do this after lights are available so materials can use the lights in shaders.
-		mat->InitScene(mergeScene);
-	}
-}
-
-void BL_Converter::RemoveScene(KX_Scene *scene)
-{
-	KX_WorldInfo *world = scene->GetWorldInfo(); // TODO move in scene.
-	if (world) {
-		delete world;
-	}
-
-#ifdef WITH_PYTHON
-	Texture::FreeAllTextures(scene);
-#endif  // WITH_PYTHON
-
-	/* Delete the meshes as some one of them depends to the data owned by the scene
-	 * e.g the display array bucket owned by the meshes and needed to be unregistered
-	 * from the bucket manager in the scene.
-	 */
-	SceneSlot& sceneSlot = m_sceneSlots[scene];
-	sceneSlot.m_meshobjects.clear();
-
-	// Delete the scene before the data, to remove all users.
-	delete scene;
-
-	m_sceneSlots.erase(scene);
+	scene->SetResources(BL_ResourceCollection(converter));
 }
 
 void BL_Converter::SetAlwaysUseExpandFraming(bool to_what)
 {
 	m_alwaysUseExpandFraming = to_what;
-}
-
-void BL_Converter::RegisterInterpolatorList(KX_Scene *scene, BL_InterpolatorList *interpolator, bAction *for_act)
-{
-	SceneSlot& sceneSlot = m_sceneSlots[scene];
-	sceneSlot.m_interpolators.emplace_back(interpolator);
-	sceneSlot.m_actionToInterp[for_act] = interpolator;
-}
-
-BL_InterpolatorList *BL_Converter::FindInterpolatorList(KX_Scene *scene, bAction *for_act)
-{
-	return m_sceneSlots[scene].m_actionToInterp[for_act];
-}
-
-void BL_Converter::RegisterMesh(KX_Scene *scene, KX_Mesh *mesh)
-{
-	m_sceneSlots[scene].m_meshobjects.emplace_back(mesh);
 }
 
 Main *BL_Converter::CreateMainDynamic(const std::string& path)
@@ -323,11 +226,11 @@ void BL_Converter::MergeAsyncLoads()
 
 	for (KX_LibLoadStatus *libload : m_mergequeue) {
 		KX_Scene *mergeScene = libload->GetMergeScene();
-		for (const BL_SceneConverter& converter : libload->GetSceneConverters()) {
+		for (BL_SceneConverter& converter : libload->GetSceneConverters()) {
 			KX_Scene *scene = converter.GetScene();
-			MergeScene(mergeScene, scene);
+			mergeScene->Merge(scene);
 			// Finalize material and mesh conversion.
-			FinalizeSceneData(converter, mergeScene);
+			converter.Finalize(mergeScene);
 			delete scene;
 		}
 
@@ -471,33 +374,32 @@ KX_LibLoadStatus *BL_Converter::LinkBlendFile(BlendHandle *blendlib, const char 
 
 	status = new KX_LibLoadStatus(this, m_ketsjiEngine, scene_merge, path);
 
-	if (idcode == ID_ME) {
-		// Convert all new meshes into BGE meshes
-		ID *mesh;
-
+	if (idcode == ID_ME || idcode == ID_AC) {
 		BL_SceneConverter sceneConverter(scene_merge);
-		for (mesh = (ID *)main_newlib->mesh.first; mesh; mesh = (ID *)mesh->next) {
-			if (options & LIB_LOAD_VERBOSE) {
-				CM_Debug("mesh name: " << mesh->name + 2);
+		if (idcode == ID_ME) {
+			// Convert all new meshes into BGE meshes
+			for (Mesh *mesh = (Mesh *)main_newlib->mesh.first; mesh; mesh = (Mesh *)mesh->id.next) {
+				if (options & LIB_LOAD_VERBOSE) {
+					CM_Debug("mesh name: " << mesh->id.name + 2);
+				}
+				BL_ConvertMesh(mesh, nullptr, scene_merge, sceneConverter);
 			}
-			KX_Mesh *meshobj = BL_ConvertMesh((Mesh *)mesh, nullptr, scene_merge, sceneConverter);
-// 			scene_merge->GetLogicManager()->RegisterMeshName(meshobj->GetName(), meshobj); TODO
+		}
+		else {
+			for (bAction *action = (bAction *)main_newlib->action.first; action; action = (bAction *)action->id.next) {
+				if (options & LIB_LOAD_VERBOSE) {
+					CM_Debug("action name: " << action->id.name + 2);
+				}
+				sceneConverter.RegisterAction(action);
+			}
 		}
 
 		// Finalize material and mesh conversion.
-		FinalizeSceneData(sceneConverter, scene_merge);
-		m_sceneSlots[scene_merge].Merge(sceneConverter);
-	}
-	else if (idcode == ID_AC) {
-		// Convert all actions
-		ID *action;
+		sceneConverter.Finalize(scene_merge);
 
-		for (action = (ID *)main_newlib->action.first; action; action = (ID *)action->next) {
-			if (options & LIB_LOAD_VERBOSE) {
-				CM_Debug("action name: " << action->name + 2);
-			}
-// 			scene_merge->GetLogicManager()->RegisterActionName(action->name + 2, action); TODO
-		}
+		// Merge ressources (mesh or actions) to the targeted scene ressource collection.
+		BL_ResourceCollection ressources(sceneConverter);
+		scene_merge->GetResources().Merge(ressources);
 	}
 	else if (idcode == ID_SCE) {
 		// Merge all new linked in scene into the existing one
@@ -528,10 +430,10 @@ KX_LibLoadStatus *BL_Converter::LinkBlendFile(BlendHandle *blendlib, const char 
 				BL_SceneConverter sceneConverter(other);
 				ConvertScene(sceneConverter, true);
 
-				MergeScene(scene_merge, other);
+				scene_merge->Merge(other);
 
 				// Finalize material and mesh conversion.
-				FinalizeSceneData(sceneConverter, scene_merge);
+				sceneConverter.Finalize(scene_merge);
 
 				delete other;
 			}
@@ -546,14 +448,18 @@ KX_LibLoadStatus *BL_Converter::LinkBlendFile(BlendHandle *blendlib, const char 
 
 		// Now handle all the actions
 		if (options & LIB_LOAD_LOAD_ACTIONS) {
-			ID *action;
+			BL_SceneConverter sceneConverter(scene_merge);
 
-			for (action = (ID *)main_newlib->action.first; action; action = (ID *)action->next) {
+			for (ID *action = (ID *)main_newlib->action.first; action; action = (ID *)action->next) {
 				if (options & LIB_LOAD_VERBOSE) {
 					CM_Debug("action name: " << action->name + 2);
 				}
-// 				scene_merge->GetLogicManager()->RegisterActionName(action->name + 2, action); TODO
+				sceneConverter.RegisterAction((bAction *)action);
 			}
+
+			// Merge ressources (mesh or actions) to the targeted scene ressource collection.
+			BL_ResourceCollection ressources(sceneConverter);
+			scene_merge->GetResources().Merge(ressources);
 		}
 	}
 
@@ -565,8 +471,6 @@ KX_LibLoadStatus *BL_Converter::LinkBlendFile(BlendHandle *blendlib, const char 
 	return status;
 }
 
-/** Note m_map_*** are all ok and don't need to be freed
- * most are temp and NewRemoveObject frees m_map_gameobject_to_blender */
 bool BL_Converter::FreeBlendFile(Main *maggie)
 {
 	// TODO use a list of file to free to avoid direct free of objects and scene when the user run python scripts.
@@ -606,114 +510,9 @@ bool BL_Converter::FreeBlendFile(Main *maggie)
 			m_ketsjiEngine->RemoveScene(scene->GetName());
 		}
 		else {
-			// in case the mesh might be refered to later
-			/*std::map<std::string, void *> &mapStringToMeshes = scene->GetLogicManager()->GetMeshMap();
-			for (std::map<std::string, void *>::iterator it = mapStringToMeshes.begin(), end = mapStringToMeshes.end(); it != end; ) {
-				KX_Mesh *meshobj = (KX_Mesh *)it->second;
-				if (meshobj && IS_TAGGED(meshobj->GetMesh())) {
-					it = mapStringToMeshes.erase(it);
-				}
-				else {
-					++it;
-				}
-			}
-
-			// Now unregister actions.
-			std::map<std::string, void *> &mapStringToActions = scene->GetLogicManager()->GetActionMap();
-			for (std::map<std::string, void *>::iterator it = mapStringToActions.begin(), end = mapStringToActions.end(); it != end; ) {
-				ID *action = (ID *)it->second;
-				if (IS_TAGGED(action)) {
-					it = mapStringToActions.erase(it);
-				}
-				else {
-					++it;
-				}
-			} TODO */
-
-			// removed tagged objects and meshes
-			std::array<EXP_ListValue<KX_GameObject> *, 2> obj_lists{{&scene->GetObjectList(), &scene->GetInactiveList()}};
-
-			for (EXP_ListValue<KX_GameObject> *obs : obj_lists) {
-				for (int ob_idx = 0; ob_idx < obs->GetCount(); ob_idx++) {
-					KX_GameObject *gameobj = obs->GetValue(ob_idx);
-					if (IS_TAGGED(gameobj->GetBlenderObject())) {
-						int size_before = obs->GetCount();
-
-						/* Eventually calls RemoveNodeDestructObject
-						 * frees m_map_gameobject_to_blender from UnregisterGameObject */
-						scene->RemoveObject(gameobj);
-
-						if (size_before != obs->GetCount()) {
-							ob_idx--;
-						}
-						else {
-							CM_Error("could not remove \"" << gameobj->GetName() << "\"");
-						}
-					}
-					else {
-						gameobj->RemoveTaggedActions();
-						// free the mesh, we could be referecing a linked one!
-
-						for (KX_Mesh *meshobj : gameobj->GetMeshList()) {
-							if (IS_TAGGED(meshobj->GetMesh())) {
-								gameobj->RemoveMeshes(); /* XXX - slack, should only remove meshes that are library items but mostly objects only have 1 mesh */
-								break;
-							}
-							else {
-								// also free the mesh if it's using a tagged material
-								for (RAS_MeshMaterial *meshmat : meshobj->GetMeshMaterialList()) {
-									if (IS_TAGGED(meshmat->GetBucket()->GetPolyMaterial()->GetBlenderMaterial())) {
-										gameobj->RemoveMeshes(); // XXX - slack, same as above
-										break;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+			scene->RemoveTagged();
 		}
 	}
-
-	for (std::map<KX_Scene *, SceneSlot>::iterator sit = m_sceneSlots.begin(), send = m_sceneSlots.end(); sit != send; ++sit) {
-		KX_Scene *scene = sit->first;
-		SceneSlot& sceneSlot = sit->second;
-
-		for (UniquePtrList<KX_Mesh>::iterator it =  sceneSlot.m_meshobjects.begin(); it !=  sceneSlot.m_meshobjects.end(); ) {
-			KX_Mesh *mesh = (*it).get();
-			if (IS_TAGGED(mesh->GetMesh())) {
-				it = sceneSlot.m_meshobjects.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
-		for (UniquePtrList<KX_BlenderMaterial>::iterator it = sceneSlot.m_materials.begin(); it != sceneSlot.m_materials.end(); ) {
-			KX_BlenderMaterial *mat = (*it).get();
-			Material *bmat = mat->GetBlenderMaterial();
-			if (IS_TAGGED(bmat)) {
-				scene->GetBucketManager()->RemoveMaterial(mat);
-				it = sceneSlot.m_materials.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
-		for (UniquePtrList<BL_InterpolatorList>::iterator it = sceneSlot.m_interpolators.begin(); it != sceneSlot.m_interpolators.end(); ) {
-			BL_InterpolatorList *interp = (*it).get();
-			bAction *action = interp->GetAction();
-			if (IS_TAGGED(action)) {
-				sceneSlot.m_actionToInterp.erase(action);
-				it = sceneSlot.m_interpolators.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-	}
-
 #ifdef WITH_PYTHON
 	/* make sure this maggie is removed from the import list if it's there
 	 * (this operation is safe if it isn't in the list) */
@@ -731,18 +530,6 @@ bool BL_Converter::FreeBlendFile(Main *maggie)
 bool BL_Converter::FreeBlendFile(const std::string& path)
 {
 	return FreeBlendFile(GetMainDynamicPath(path));
-}
-
-void BL_Converter::MergeScene(KX_Scene *to, KX_Scene *from)
-{
-	to->MergeScene(from);
-
-	m_sceneSlots[to].Merge(m_sceneSlots[from]);
-	m_sceneSlots.erase(from);
-
-	// Delete from scene's world info.
-	delete from->GetWorldInfo();
-	from->SetWorldInfo(nullptr);
 }
 
 /** This function merges a mesh from the current scene into another main
@@ -822,18 +609,19 @@ KX_Mesh *BL_Converter::ConvertMeshSpecial(KX_Scene *kx_scene, Main *maggie, cons
 	BL_SceneConverter sceneConverter(kx_scene);
 
 	KX_Mesh *meshobj = BL_ConvertMesh((Mesh *)me, nullptr, kx_scene, sceneConverter);
-// 	kx_scene->GetLogicManager()->RegisterMeshName(meshobj->GetName(), meshobj); TODO
 
 	// Finalize material and mesh conversion.
-	FinalizeSceneData(sceneConverter, kx_scene);
-	m_sceneSlots[kx_scene].Merge(sceneConverter);
+	sceneConverter.Finalize(kx_scene);
+	BL_ResourceCollection ressources(sceneConverter);
+	kx_scene->GetResources().Merge(ressources);
 
 	return meshobj;
 }
 
 void BL_Converter::PrintStats()
 {
-	CM_Message("BGE STATS");
+	// TODO
+	/*CM_Message("BGE STATS");
 	CM_Message(std::endl << "Assets:");
 
 	unsigned int nummat = 0;
@@ -858,5 +646,5 @@ void BL_Converter::PrintStats()
 	CM_Message("\t scenes: " << m_sceneSlots.size());
 	CM_Message("\t materials: " << nummat);
 	CM_Message("\t meshes: " << nummesh);
-	CM_Message("\t interpolators: " << numinter);
+	CM_Message("\t interpolators: " << numinter);*/
 }

source/gameengine/VideoTexture/ImageRender.cpp
@@ -239,14 +239,14 @@ bool ImageRender::Render()
 	if (m_mirror) {
 		// mirror mode, compute camera frustum, position and orientation
 		// convert mirror position and normal in world space
-		const mt::mat3 & mirrorObjWorldOri = m_mirror->GetSGNode()->GetWorldOrientation();
-		const mt::vec3 & mirrorObjWorldPos = m_mirror->GetSGNode()->GetWorldPosition();
-		const mt::vec3 & mirrorObjWorldScale = m_mirror->GetSGNode()->GetWorldScaling();
+		const mt::mat3 & mirrorObjWorldOri = m_mirror->NodeGetWorldOrientation();
+		const mt::vec3 & mirrorObjWorldPos = m_mirror->NodeGetWorldPosition();
+		const mt::vec3 & mirrorObjWorldScale = m_mirror->NodeGetWorldScaling();
 		mt::vec3 mirrorWorldPos =
 			mirrorObjWorldPos + mirrorObjWorldScale * (mirrorObjWorldOri * m_mirrorPos);
 		mt::vec3 mirrorWorldZ = mirrorObjWorldOri * m_mirrorZ;
 		// get observer world position
-		const mt::vec3 & observerWorldPos = m_observer->GetSGNode()->GetWorldPosition();
+		const mt::vec3 & observerWorldPos = m_observer->NodeGetWorldPosition();
 		// get plane D term = mirrorPos . normal
 		float mirrorPlaneDTerm = mt::dot(mirrorWorldPos, mirrorWorldZ);
 		// compute distance of observer to mirror = D - observerPos . normal
@@ -257,7 +257,7 @@ bool ImageRender::Render()
 		}
 		// set camera world position = observerPos + normal * 2 * distance
 		mt::vec3 cameraWorldPos = observerWorldPos + (2.0f * observerDistance) * mirrorWorldZ;
-		m_camera->GetSGNode()->SetLocalPosition(cameraWorldPos);
+		m_camera->NodeSetLocalPosition(cameraWorldPos);
 		// set camera orientation: z=normal, y=mirror_up in world space, x= y x z
 		mt::vec3 mirrorWorldY = mirrorObjWorldOri * m_mirrorY;
 		mt::vec3 mirrorWorldX = mirrorObjWorldOri * m_mirrorX;
@@ -265,8 +265,8 @@ bool ImageRender::Render()
 			mirrorWorldX[0], mirrorWorldY[0], mirrorWorldZ[0],
 			mirrorWorldX[1], mirrorWorldY[1], mirrorWorldZ[1],
 			mirrorWorldX[2], mirrorWorldY[2], mirrorWorldZ[2]);
-		m_camera->GetSGNode()->SetLocalOrientation(cameraWorldOri);
-		m_camera->GetSGNode()->UpdateWorldData();
+		m_camera->NodeSetLocalOrientation(cameraWorldOri);
+		m_camera->NodeUpdateGS();
 		// compute camera frustum:
 		//   get position of mirror relative to camera: offset = mirrorPos-cameraPos
 		mt::vec3 mirrorOffset = mirrorWorldPos - cameraWorldPos;
@@ -764,7 +764,7 @@ static int ImageMirror_init(PyObject *pySelf, PyObject *args, PyObject *kwds)
 
 		// get observer pointer
 		KX_GameObject *observerPtr(nullptr);
-		if (!ConvertPythonToGameObject(0, observer, &observerPtr, false, "")) {
+		if (!ConvertPythonToGameObject(scenePtr, observer, &observerPtr, false, "")) {
 			THRWEXCP(ObserverInvalid, S_OK);
 		}
 
@@ -774,7 +774,7 @@ static int ImageMirror_init(PyObject *pySelf, PyObject *args, PyObject *kwds)
 
 		// get mirror pointer
 		KX_GameObject *mirrorPtr(nullptr);
-		if (!ConvertPythonToGameObject(0, mirror, &mirrorPtr, false, "")) {
+		if (!ConvertPythonToGameObject(scenePtr, mirror, &mirrorPtr, false, "")) {
 			THRWEXCP(MirrorInvalid, S_OK);
 		}
 

UPBGE/upbge

source/gameengine/Converter/BL_Converter.cpp
@@ -36,7 +36,6 @@
 #include "KX_Scene.h"
 #include "KX_GameObject.h"
 #include "KX_Mesh.h"
-#include "RAS_BucketManager.h"
 #include "KX_PhysicsEngineEnums.h"
 #include "KX_KetsjiEngine.h"
 #include "KX_PythonInit.h" // So we can handle adding new text datablocks for Python to import
@@ -48,11 +47,15 @@
 #include "BL_ConvertObjectInfo.h"
 #include "KX_BlenderMaterial.h"
 
-#include "EXP_PropString.h"
+#include "RAS_BucketManager.h"
 
-#ifdef WITH_PYTHON
-#  include "Texture.h" // For FreeAllTextures.
-#endif  // WITH_PYTHON
+#include "LA_SystemCommandLine.h"
+
+#include "DummyPhysicsEnvironment.h"
+
+#ifdef WITH_BULLET
+#  include "CcdPhysicsEnvironment.h"
+#endif
 
 // This list includes only data type definitions
 #include "DNA_scene_types.h"
@@ -77,48 +80,6 @@ extern "C" {
 #include "CM_Message.h"
 
 #include <cstring>
-#include <memory>
-
-BL_Converter::SceneSlot::SceneSlot() = default;
-
-BL_Converter::SceneSlot::SceneSlot(const BL_SceneConverter& converter)
-{
-	Merge(converter);
-}
-
-BL_Converter::SceneSlot::~SceneSlot() = default;
-
-void BL_Converter::SceneSlot::Merge(BL_Converter::SceneSlot& other)
-{
-	m_materials.insert(m_materials.begin(),
-	                   std::make_move_iterator(other.m_materials.begin()),
-	                   std::make_move_iterator(other.m_materials.end()));
-	m_meshobjects.insert(m_meshobjects.begin(),
-	                     std::make_move_iterator(other.m_meshobjects.begin()),
-	                     std::make_move_iterator(other.m_meshobjects.end()));
-	m_objectInfos.insert(m_objectInfos.begin(),
-	                     std::make_move_iterator(other.m_objectInfos.begin()),
-	                     std::make_move_iterator(other.m_objectInfos.end()));
-	m_actions.insert(m_actions.begin(),
-					 std::make_move_iterator(other.m_actions.begin()),
-					 std::make_move_iterator(other.m_actions.end()));
-}
-
-void BL_Converter::SceneSlot::Merge(const BL_SceneConverter& converter)
-{
-	for (KX_BlenderMaterial *mat : converter.m_materials) {
-		m_materials.emplace_back(mat);
-	}
-	for (KX_Mesh *meshobj : converter.m_meshobjects) {
-		m_meshobjects.emplace_back(meshobj);
-	}
-	for (BL_ConvertObjectInfo *info : converter.m_objectInfos) {
-		m_objectInfos.emplace_back(info);
-	}
-	for (BL_ActionData *action : converter.m_actions) {
-		m_actions.emplace_back(action);
-	}
-}
 
 BL_Converter::BL_Converter(Main *maggie, KX_KetsjiEngine *engine, bool alwaysUseExpandFraming, float camZoom)
 	:m_maggie(maggie),
@@ -177,7 +138,6 @@ void BL_Converter::ConvertScene(KX_Scene *scene)
 	BL_SceneConverter converter(scene, BL_Resource::Library(m_maggie));
 	ConvertScene(converter, false, true);
 	PostConvertScene(converter);
-	m_sceneSlots.emplace(scene, converter);
 	ReloadShaders(scene);
 }
 
@@ -195,40 +155,17 @@ void BL_Converter::ConvertScene(BL_SceneConverter& converter, bool libloading, b
 		m_alwaysUseExpandFraming,
 		m_camZoom,
 		libloading);
-
 	// Handle actions.
 	if (actions) {
 		BL_ConvertActions(scene, m_maggie, converter);
 	}
+
+	scene->SetResources(BL_ResourceCollection(converter));
 }
 
 void BL_Converter::PostConvertScene(const BL_SceneConverter& converter)
 {
 	BL_PostConvertBlenderObjects(converter.GetScene(), converter);
-}
-
-void BL_Converter::RemoveScene(KX_Scene *scene)
-{
-#ifdef WITH_PYTHON
-	Texture::FreeAllTextures(scene);
-#endif  // WITH_PYTHON
-
-	/* Delete the meshes as some one of them depends to the data owned by the scene
-	 * e.g the display array bucket owned by the meshes and needed to be unregistered
-	 * from the bucket manager in the scene.
-	 */
-	SceneSlot& sceneSlot = m_sceneSlots[scene];
-	sceneSlot.m_meshobjects.clear();
-
-	// Delete the scene before the data, to remove all users.
-	delete scene;
-
-	m_sceneSlots.erase(scene);
-}
-
-void BL_Converter::RegisterMesh(KX_Scene *scene, KX_Mesh *mesh)
-{
-	m_sceneSlots[scene].m_meshobjects.emplace_back(mesh);
 }
 
 Main *BL_Converter::CreateLibrary(const std::string& path)
@@ -535,8 +472,9 @@ bool BL_Converter::FreeBlendFileData(Main *maggie)
 		return false;
 	}
 
+	EXP_ListValue<KX_Scene>& scenes = m_ketsjiEngine->GetScenes();
 	// For each scene try to remove any usage of ressources from the library.
-	for (KX_Scene *scene : m_ketsjiEngine->GetScenes()) {
+	for (KX_Scene *scene : scenes) {
 		// Both list containing all the scene objects.
 		std::array<EXP_ListValue<KX_GameObject> *, 2> allObjects{{&scene->GetObjectList(), &scene->GetInactiveList()}};
 
@@ -560,61 +498,8 @@ bool BL_Converter::FreeBlendFileData(Main *maggie)
 		}
 
 		scene->RemoveEuthanasyObjects();
-	}
 
-	// Free ressources belonging to the library and unregister them.
-	for (auto& pair : m_sceneSlots) {
-		KX_Scene *scene = pair.first;
-// 		SCA_LogicManager *logicmgr = scene->GetLogicManager(); TODO
-		SceneSlot& sceneSlot = pair.second;
-
-		// Free meshes.
-		for (UniquePtrList<KX_Mesh>::iterator it =  sceneSlot.m_meshobjects.begin(); it !=  sceneSlot.m_meshobjects.end(); ) {
-			KX_Mesh *mesh = it->get();
-			if (mesh->Belong(libraryId)) {
-// 				logicmgr->UnregisterMesh(mesh); TODO
-				it = sceneSlot.m_meshobjects.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
-		// Free materials.
-		for (UniquePtrList<KX_BlenderMaterial>::iterator it = sceneSlot.m_materials.begin(); it != sceneSlot.m_materials.end(); ) {
-			KX_BlenderMaterial *mat = it->get();
-			if (mat->Belong(libraryId)) {
-				scene->GetBucketManager()->RemoveMaterial(mat);
-				it = sceneSlot.m_materials.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
-		// Free actions.
-		for (UniquePtrList<BL_ActionData>::iterator it = sceneSlot.m_actions.begin(); it != sceneSlot.m_actions.end(); ) {
-			BL_ActionData *act = it->get();
-			if (act->Belong(libraryId)) {
-// 				logicmgr->UnregisterAction(act); TODO
-				it = sceneSlot.m_actions.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
-		// Free object infos.
-		for (UniquePtrList<BL_ConvertObjectInfo>::iterator it = sceneSlot.m_objectInfos.begin(); it != sceneSlot.m_objectInfos.end(); ) {
-			BL_ConvertObjectInfo *info = it->get();
-			if (info->Belong(libraryId)) {
-				it = sceneSlot.m_objectInfos.erase(it);
-			}
-			else {
-				++it;
-			}
-		}
-
+		scene->GetResources().RemoveResources(libraryId, scene);
 		// Reload materials cause they used lamps removed now.
 		scene->GetBucketManager()->ReloadMaterials();
 	}
@@ -657,7 +542,7 @@ bool BL_Converter::FreeBlendFile(const std::string& path)
 
 void BL_Converter::MergeSceneData(KX_Scene *to, const BL_SceneConverter& converter)
 {
-	for (KX_Mesh *mesh : converter.m_meshobjects) {
+	for (KX_Mesh *mesh : converter.m_meshes) {
 		mesh->ReplaceScene(to);
 	}
 
@@ -666,7 +551,8 @@ void BL_Converter::MergeSceneData(KX_Scene *to, const BL_SceneConverter& convert
 		mat->ReplaceScene(to);
 	}
 
-	m_sceneSlots[to].Merge(converter);
+	BL_ResourceCollection ressources(converter);
+	to->GetResources().Merge(ressources);
 }
 
 void BL_Converter::MergeScene(KX_Scene *to, const BL_SceneConverter& converter)
@@ -676,7 +562,7 @@ void BL_Converter::MergeScene(KX_Scene *to, const BL_SceneConverter& converter)
 	MergeSceneData(to, converter);
 
 	KX_Scene *from = converter.GetScene();
-	to->MergeScene(from);
+	to->Merge(from);
 
 	ReloadShaders(to);
 
@@ -685,7 +571,7 @@ void BL_Converter::MergeScene(KX_Scene *to, const BL_SceneConverter& converter)
 
 void BL_Converter::ReloadShaders(KX_Scene *scene)
 {
-	for (std::unique_ptr<KX_BlenderMaterial>& mat : m_sceneSlots[scene].m_materials) {
+	for (std::unique_ptr<KX_BlenderMaterial>& mat : scene->GetResources().m_materials) {
 		mat->ReloadMaterial();
 	}
 
@@ -774,7 +660,8 @@ KX_Mesh *BL_Converter::ConvertMeshSpecial(KX_Scene *kx_scene, Main *maggie, cons
 
 	KX_Mesh *meshobj = BL_ConvertMesh((Mesh *)me, nullptr, kx_scene, sceneConverter);
 
-	MergeSceneData(kx_scene, sceneConverter);
+	BL_ResourceCollection ressources(sceneConverter);
+	kx_scene->GetResources().Merge(ressources);
 	ReloadShaders(sceneConverter);
 
 	return meshobj;
@@ -782,7 +669,8 @@ KX_Mesh *BL_Converter::ConvertMeshSpecial(KX_Scene *kx_scene, Main *maggie, cons
 
 void BL_Converter::PrintStats()
 {
-	CM_Message("BGE STATS");
+	// TODO
+	/*CM_Message("BGE STATS");
 	CM_Message(std::endl << "Assets:");
 
 	unsigned int nummat = 0;
@@ -808,4 +696,5 @@ void BL_Converter::PrintStats()
 	CM_Message("\t materials: " << nummat);
 	CM_Message("\t meshes: " << nummesh);
 	CM_Message("\t actions: " << numacts);
+	*/
 }

source/gameengine/VideoTexture/ImageRender.cpp
@@ -239,14 +239,14 @@ bool ImageRender::Render()
 	if (m_mirror) {
 		// mirror mode, compute camera frustum, position and orientation
 		// convert mirror position and normal in world space
-		const mt::mat3 & mirrorObjWorldOri = m_mirror->GetNode()->GetWorldOrientation();
-		const mt::vec3 & mirrorObjWorldPos = m_mirror->GetNode()->GetWorldPosition();
-		const mt::vec3 & mirrorObjWorldScale = m_mirror->GetNode()->GetWorldScaling();
+		const mt::mat3 & mirrorObjWorldOri = m_mirror->NodeGetWorldOrientation();
+		const mt::vec3 & mirrorObjWorldPos = m_mirror->NodeGetWorldPosition();
+		const mt::vec3 & mirrorObjWorldScale = m_mirror->NodeGetWorldScaling();
 		mt::vec3 mirrorWorldPos =
 			mirrorObjWorldPos + mirrorObjWorldScale * (mirrorObjWorldOri * m_mirrorPos);
 		mt::vec3 mirrorWorldZ = mirrorObjWorldOri * m_mirrorZ;
 		// get observer world position
-		const mt::vec3 & observerWorldPos = m_observer->GetNode()->GetWorldPosition();
+		const mt::vec3 & observerWorldPos = m_observer->NodeGetWorldPosition();
 		// get plane D term = mirrorPos . normal
 		float mirrorPlaneDTerm = mt::dot(mirrorWorldPos, mirrorWorldZ);
 		// compute distance of observer to mirror = D - observerPos . normal
@@ -257,7 +257,7 @@ bool ImageRender::Render()
 		}
 		// set camera world position = observerPos + normal * 2 * distance
 		mt::vec3 cameraWorldPos = observerWorldPos + (2.0f * observerDistance) * mirrorWorldZ;
-		m_camera->GetNode()->SetLocalPosition(cameraWorldPos);
+		m_camera->NodeSetLocalPosition(cameraWorldPos);
 		// set camera orientation: z=normal, y=mirror_up in world space, x= y x z
 		mt::vec3 mirrorWorldY = mirrorObjWorldOri * m_mirrorY;
 		mt::vec3 mirrorWorldX = mirrorObjWorldOri * m_mirrorX;
@@ -265,8 +265,8 @@ bool ImageRender::Render()
 			mirrorWorldX[0], mirrorWorldY[0], mirrorWorldZ[0],
 			mirrorWorldX[1], mirrorWorldY[1], mirrorWorldZ[1],
 			mirrorWorldX[2], mirrorWorldY[2], mirrorWorldZ[2]);
-		m_camera->GetNode()->SetLocalOrientation(cameraWorldOri);
-		m_camera->GetNode()->UpdateWorldData();
+		m_camera->NodeSetLocalOrientation(cameraWorldOri);
+		m_camera->NodeUpdate();
 		// compute camera frustum:
 		//   get position of mirror relative to camera: offset = mirrorPos-cameraPos
 		mt::vec3 mirrorOffset = mirrorWorldPos - cameraWorldPos;
@@ -763,7 +763,7 @@ static int ImageMirror_init(PyObject *pySelf, PyObject *args, PyObject *kwds)
 
 		// get observer pointer
 		KX_GameObject *observerPtr(nullptr);
-		if (!ConvertPythonToGameObject(0, observer, &observerPtr, false, "")) {
+		if (!ConvertPythonToGameObject(scenePtr, observer, &observerPtr, false, "")) {
 			THRWEXCP(ObserverInvalid, S_OK);
 		}
 
@@ -773,7 +773,7 @@ static int ImageMirror_init(PyObject *pySelf, PyObject *args, PyObject *kwds)
 
 		// get mirror pointer
 		KX_GameObject *mirrorPtr(nullptr);
-		if (!ConvertPythonToGameObject(0, mirror, &mirrorPtr, false, "")) {
+		if (!ConvertPythonToGameObject(scenePtr, mirror, &mirrorPtr, false, "")) {
 			THRWEXCP(MirrorInvalid, S_OK);
 		}
 