thatimster/blender

UPBGE: Code refactor to work with opengl compatibility mode in MacOs
It includes:
- Only set gl_ClipDistance for ATI cards
- Using compatible functions and input/outputs for glsl 1.2
- Use APPLE_vertex_array_object extension for MacosX
- Unset OpenColorIO and OpenSubDiv as they provoque freezes
- New GPU funtions to manage VAOs
- Use #ifdef instead of calling function continuosly

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLDebugDraw.cpp
@@ -32,6 +32,7 @@
 #include "GPU_material.h"
 #include "GPU_glew.h"
 #include "GPU_shader.h"
+#include "GPU_vertex.h"
 
 extern "C" {
 #  include "BLF_api.h"
@@ -105,9 +106,8 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 	m_frustumSolidShader = GPU_shader_get_builtin_shader(GPU_SHADER_FRUSTUM_SOLID);
 	m_box2dShader = GPU_shader_get_builtin_shader(GPU_SHADER_2D_BOX);
 
-	glGenVertexArrays(MAX_VAO, m_vaos);
-
-	glBindVertexArray(m_vaos[LINES_VAO]);
+	GPU_gen_vertex_arrays(MAX_VAO, m_vaos);
+	GPU_bind_vertex_array(m_vaos[LINES_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Line) / 2;
 		const unsigned int pos = GPU_shader_get_attribute(m_colorShader, "pos");
@@ -120,7 +120,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 
 	static const unsigned short frustumStride = sizeof(RAS_DebugDraw::Frustum);
 
-	glBindVertexArray(m_vaos[FRUSTUMS_LINE_VAO]);
+	GPU_bind_vertex_array(m_vaos[FRUSTUMS_LINE_VAO]);
 	{
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumLineShader, "pos");
 		const unsigned short mat = GPU_shader_get_attribute(m_frustumLineShader, "mat");
@@ -136,7 +136,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[FRUSTUMS_SOLID_VAO]);
+	GPU_bind_vertex_array(m_vaos[FRUSTUMS_SOLID_VAO]);
 	{
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumSolidShader, "pos");
 		const unsigned short mat = GPU_shader_get_attribute(m_frustumSolidShader, "mat");
@@ -154,7 +154,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[AABB_VAO]);
+	GPU_bind_vertex_array(m_vaos[AABB_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Aabb);
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumLineShader, "pos");
@@ -171,7 +171,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[BOX_2D_VAO]);
+	GPU_bind_vertex_array(m_vaos[BOX_2D_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Box2d);
 		const unsigned short pos = GPU_shader_get_attribute(m_box2dShader, "pos");
@@ -186,7 +186,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		attribVector(trans, stride, offsetof(RAS_DebugDraw::Box2d, m_trans), 4, 1);
 	}
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
@@ -194,7 +194,7 @@ RAS_OpenGLDebugDraw::~RAS_OpenGLDebugDraw()
 {
 	glDeleteBuffers(MAX_IBO, m_ibos);
 	glDeleteBuffers(MAX_VBO, m_vbos);
-	glDeleteVertexArrays(MAX_VAO, m_vaos);
+	GPU_delete_vertex_arrays(MAX_VAO, m_vaos);
 }
 
 void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_DebugDraw *debugDraw)
@@ -209,7 +209,7 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numlines > 0) {
 		updateVbo(m_vbos[LINES_VBO], lines);
 
-		glBindVertexArray(m_vaos[LINES_VAO]);
+		GPU_bind_vertex_array(m_vaos[LINES_VAO]);
 		GPU_shader_bind(m_colorShader);
 		glDrawArrays(GL_LINES, 0, numlines * 2);
 	}
@@ -219,11 +219,11 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numfrustums > 0) {
 		updateVbo(m_vbos[FRUSTUMS_VBO], frustums);
 
-		glBindVertexArray(m_vaos[FRUSTUMS_LINE_VAO]);
+		GPU_bind_vertex_array(m_vaos[FRUSTUMS_LINE_VAO]);
 		GPU_shader_bind(m_frustumLineShader);
 		glDrawElementsInstancedARB(GL_LINES, 24, GL_UNSIGNED_BYTE, nullptr, numfrustums);
 
-		glBindVertexArray(m_vaos[FRUSTUMS_SOLID_VAO]);
+		GPU_bind_vertex_array(m_vaos[FRUSTUMS_SOLID_VAO]);
 		GPU_shader_bind(m_frustumSolidShader);
 		glDrawElementsInstancedARB(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, (const void *)(sizeof(GLubyte) * 24), numfrustums);
 	}
@@ -233,7 +233,7 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numaabbs > 0) {
 		updateVbo(m_vbos[AABB_VBO], aabbs);
 
-		glBindVertexArray(m_vaos[AABB_VAO]);
+		GPU_bind_vertex_array(m_vaos[AABB_VAO]);
 		GPU_shader_bind(m_frustumLineShader);
 		glDrawElementsInstancedARB(GL_LINES, 24, GL_UNSIGNED_BYTE, nullptr, numaabbs);
 	}
@@ -258,12 +258,12 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numboxes > 0) {
 		updateVbo(m_vbos[BOX_2D_VBO], boxes2d);
 
-		glBindVertexArray(m_vaos[BOX_2D_VAO]);
+		GPU_bind_vertex_array(m_vaos[BOX_2D_VAO]);
 		GPU_shader_bind(m_box2dShader);
 		glDrawArraysInstancedARB(GL_TRIANGLE_FAN, 0, 4, numboxes);
 	}
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	GPU_shader_unbind();
 

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
@@ -40,6 +40,7 @@
 #include "GPU_extensions.h"
 #include "GPU_material.h"
 #include "GPU_shader.h"
+#include "GPU_vertex.h"
 
 extern "C" {
 #  include "BLF_api.h"
@@ -111,7 +112,7 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Generate the VBO and IBO for screen overlay plane.
 	glGenBuffers(1, &m_vbo);
 	glGenBuffers(1, &m_ibo);
-	glGenVertexArrays(1, &m_vao);
+	GPU_gen_vertex_arrays(1, &m_vao);
 
 	// Vertexes for screen plane, it contains the vertex position (3 floats) and the vertex uv after (2 floats, total size = 5 floats).
 	static const float vertices[] = {
@@ -124,7 +125,7 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Indices for screen plane.
 	static const GLubyte indices[] = {3, 2, 1, 0};
 
-	glBindVertexArray(m_vao);
+	GPU_bind_vertex_array(m_vao);
 
 	// Send indices in the sreen plane IBO.
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
@@ -145,24 +146,24 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Unbind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_OpenGLRasterizer::ScreenPlane::~ScreenPlane()
 {
 	// Delete screen overlay plane VBO/IBO/VAO
-	glDeleteVertexArrays(1, &m_vao);
+	GPU_delete_vertex_arrays(1, &m_vao);
 	glDeleteBuffers(1, &m_vbo);
 	glDeleteBuffers(1, &m_ibo);
 }
 
 inline void RAS_OpenGLRasterizer::ScreenPlane::Render()
 {
-	glBindVertexArray(m_vao);
+	GPU_bind_vertex_array(m_vao);
 	// Draw in triangle fan mode to reduce IBO size.
 	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_BYTE, 0);
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_OpenGLRasterizer::RAS_OpenGLRasterizer(RAS_Rasterizer *rasterizer)

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_StorageVao.cpp
@@ -24,6 +24,7 @@
 #include "RAS_DisplayArrayStorage.h"
 #include "RAS_StorageVao.h"
 #include "RAS_StorageVbo.h"
+#include "GPU_vertex.h"
 
 struct AttribData {
 	int size;
@@ -42,8 +43,8 @@ static const AttribData attribData[RAS_AttributeArray::RAS_ATTRIB_MAX] = {
 RAS_StorageVao::RAS_StorageVao(const RAS_DisplayArrayLayout &layout, RAS_DisplayArrayStorage *arrayStorage,
                                const RAS_AttributeArray::AttribList& attribList)
 {
-	glGenVertexArrays(1, &m_id);
-	glBindVertexArray(m_id);
+	GPU_gen_vertex_arrays(1, &m_id);
+	GPU_bind_vertex_array(m_id);
 
 	RAS_StorageVbo *vbo = arrayStorage->GetVbo();
 	vbo->BindVertexBuffer();
@@ -113,20 +114,20 @@ RAS_StorageVao::RAS_StorageVao(const RAS_DisplayArrayLayout &layout, RAS_Display
 	// VBO are not tracked by the VAO excepted for IBO.
 	vbo->UnbindVertexBuffer();
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_StorageVao::~RAS_StorageVao()
 {
-	glDeleteVertexArrays(1, &m_id);
+	GPU_delete_vertex_arrays(1, &m_id);
 }
 
 void RAS_StorageVao::BindPrimitives()
 {
-	glBindVertexArray(m_id);
+	GPU_bind_vertex_array(m_id);
 }
 
 void RAS_StorageVao::UnbindPrimitives()
 {
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }

UPBGE/upbge

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLDebugDraw.cpp
@@ -32,6 +32,7 @@
 #include "GPU_material.h"
 #include "GPU_glew.h"
 #include "GPU_shader.h"
+#include "GPU_vertex_array.h"
 
 extern "C" {
 #  include "BLF_api.h"
@@ -105,9 +106,8 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 	m_frustumSolidShader = GPU_shader_get_builtin_shader(GPU_SHADER_FRUSTUM_SOLID);
 	m_box2dShader = GPU_shader_get_builtin_shader(GPU_SHADER_2D_BOX);
 
-	glGenVertexArrays(MAX_VAO, m_vaos);
-
-	glBindVertexArray(m_vaos[LINES_VAO]);
+	GPU_create_vertex_arrays(MAX_VAO, m_vaos);
+	GPU_bind_vertex_array(m_vaos[LINES_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Line) / 2;
 		const unsigned int pos = GPU_shader_get_attribute(m_colorShader, "pos");
@@ -120,7 +120,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 
 	static const unsigned short frustumStride = sizeof(RAS_DebugDraw::Frustum);
 
-	glBindVertexArray(m_vaos[FRUSTUMS_LINE_VAO]);
+	GPU_bind_vertex_array(m_vaos[FRUSTUMS_LINE_VAO]);
 	{
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumLineShader, "pos");
 		const unsigned short mat = GPU_shader_get_attribute(m_frustumLineShader, "mat");
@@ -136,7 +136,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[FRUSTUMS_SOLID_VAO]);
+	GPU_bind_vertex_array(m_vaos[FRUSTUMS_SOLID_VAO]);
 	{
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumSolidShader, "pos");
 		const unsigned short mat = GPU_shader_get_attribute(m_frustumSolidShader, "mat");
@@ -154,7 +154,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[AABB_VAO]);
+	GPU_bind_vertex_array(m_vaos[AABB_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Aabb);
 		const unsigned short pos = GPU_shader_get_attribute(m_frustumLineShader, "pos");
@@ -171,7 +171,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibos[BOX_IBO]);
 	}
 
-	glBindVertexArray(m_vaos[BOX_2D_VAO]);
+	GPU_bind_vertex_array(m_vaos[BOX_2D_VAO]);
 	{
 		static const unsigned short stride = sizeof(RAS_DebugDraw::Box2d);
 		const unsigned short pos = GPU_shader_get_attribute(m_box2dShader, "pos");
@@ -186,7 +186,7 @@ RAS_OpenGLDebugDraw::RAS_OpenGLDebugDraw()
 		attribVector(trans, stride, offsetof(RAS_DebugDraw::Box2d, m_trans), 4, 1);
 	}
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
@@ -194,7 +194,7 @@ RAS_OpenGLDebugDraw::~RAS_OpenGLDebugDraw()
 {
 	glDeleteBuffers(MAX_IBO, m_ibos);
 	glDeleteBuffers(MAX_VBO, m_vbos);
-	glDeleteVertexArrays(MAX_VAO, m_vaos);
+	GPU_delete_vertex_arrays(MAX_VAO, m_vaos);
 }
 
 void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_DebugDraw *debugDraw)
@@ -209,7 +209,7 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numlines > 0) {
 		updateVbo(m_vbos[LINES_VBO], lines);
 
-		glBindVertexArray(m_vaos[LINES_VAO]);
+		GPU_bind_vertex_array(m_vaos[LINES_VAO]);
 		GPU_shader_bind(m_colorShader);
 		glDrawArrays(GL_LINES, 0, numlines * 2);
 	}
@@ -219,11 +219,11 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numfrustums > 0) {
 		updateVbo(m_vbos[FRUSTUMS_VBO], frustums);
 
-		glBindVertexArray(m_vaos[FRUSTUMS_LINE_VAO]);
+		GPU_bind_vertex_array(m_vaos[FRUSTUMS_LINE_VAO]);
 		GPU_shader_bind(m_frustumLineShader);
 		glDrawElementsInstancedARB(GL_LINES, 24, GL_UNSIGNED_BYTE, nullptr, numfrustums);
 
-		glBindVertexArray(m_vaos[FRUSTUMS_SOLID_VAO]);
+		GPU_bind_vertex_array(m_vaos[FRUSTUMS_SOLID_VAO]);
 		GPU_shader_bind(m_frustumSolidShader);
 		glDrawElementsInstancedARB(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, (const void *)(sizeof(GLubyte) * 24), numfrustums);
 	}
@@ -233,7 +233,7 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numaabbs > 0) {
 		updateVbo(m_vbos[AABB_VBO], aabbs);
 
-		glBindVertexArray(m_vaos[AABB_VAO]);
+		GPU_bind_vertex_array(m_vaos[AABB_VAO]);
 		GPU_shader_bind(m_frustumLineShader);
 		glDrawElementsInstancedARB(GL_LINES, 24, GL_UNSIGNED_BYTE, nullptr, numaabbs);
 	}
@@ -258,12 +258,12 @@ void RAS_OpenGLDebugDraw::Flush(RAS_Rasterizer *rasty, RAS_ICanvas *canvas, RAS_
 	if (numboxes > 0) {
 		updateVbo(m_vbos[BOX_2D_VBO], boxes2d);
 
-		glBindVertexArray(m_vaos[BOX_2D_VAO]);
+		GPU_bind_vertex_array(m_vaos[BOX_2D_VAO]);
 		GPU_shader_bind(m_box2dShader);
 		glDrawArraysInstancedARB(GL_TRIANGLE_FAN, 0, 4, numboxes);
 	}
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	GPU_shader_unbind();
 

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_OpenGLRasterizer.cpp
@@ -40,6 +40,7 @@
 #include "GPU_extensions.h"
 #include "GPU_material.h"
 #include "GPU_shader.h"
+#include "GPU_vertex_array.h"
 
 extern "C" {
 #  include "BLF_api.h"
@@ -111,7 +112,7 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Generate the VBO and IBO for screen overlay plane.
 	glGenBuffers(1, &m_vbo);
 	glGenBuffers(1, &m_ibo);
-	glGenVertexArrays(1, &m_vao);
+	GPU_create_vertex_arrays(1, &m_vao);
 
 	// Vertexes for screen plane, it contains the vertex position (3 floats) and the vertex uv after (2 floats, total size = 5 floats).
 	static const float vertices[] = {
@@ -124,7 +125,7 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Indices for screen plane.
 	static const GLubyte indices[] = {3, 2, 1, 0};
 
-	glBindVertexArray(m_vao);
+	GPU_bind_vertex_array(m_vao);
 
 	// Send indices in the sreen plane IBO.
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
@@ -145,24 +146,24 @@ RAS_OpenGLRasterizer::ScreenPlane::ScreenPlane()
 	// Unbind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_OpenGLRasterizer::ScreenPlane::~ScreenPlane()
 {
 	// Delete screen overlay plane VBO/IBO/VAO
-	glDeleteVertexArrays(1, &m_vao);
+	GPU_delete_vertex_arrays(1, &m_vao);
 	glDeleteBuffers(1, &m_vbo);
 	glDeleteBuffers(1, &m_ibo);
 }
 
 inline void RAS_OpenGLRasterizer::ScreenPlane::Render()
 {
-	glBindVertexArray(m_vao);
+	GPU_bind_vertex_array(m_vao);
 	// Draw in triangle fan mode to reduce IBO size.
 	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_BYTE, 0);
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_OpenGLRasterizer::RAS_OpenGLRasterizer(RAS_Rasterizer *rasterizer)

source/gameengine/Rasterizer/RAS_OpenGLRasterizer/RAS_StorageVao.cpp
@@ -24,6 +24,7 @@
 #include "RAS_DisplayArrayStorage.h"
 #include "RAS_StorageVao.h"
 #include "RAS_StorageVbo.h"
+#include "GPU_vertex_array.h"
 
 struct AttribData {
 	int size;
@@ -42,8 +43,8 @@ static const AttribData attribData[RAS_AttributeArray::RAS_ATTRIB_MAX] = {
 RAS_StorageVao::RAS_StorageVao(const RAS_DisplayArrayLayout &layout, RAS_DisplayArrayStorage *arrayStorage,
                                const RAS_AttributeArray::AttribList& attribList)
 {
-	glGenVertexArrays(1, &m_id);
-	glBindVertexArray(m_id);
+	GPU_create_vertex_arrays(1, &m_id);
+	GPU_bind_vertex_array(m_id);
 
 	RAS_StorageVbo *vbo = arrayStorage->GetVbo();
 	vbo->BindVertexBuffer();
@@ -113,20 +114,20 @@ RAS_StorageVao::RAS_StorageVao(const RAS_DisplayArrayLayout &layout, RAS_Display
 	// VBO are not tracked by the VAO excepted for IBO.
 	vbo->UnbindVertexBuffer();
 
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }
 
 RAS_StorageVao::~RAS_StorageVao()
 {
-	glDeleteVertexArrays(1, &m_id);
+	GPU_delete_vertex_arrays(1, &m_id);
 }
 
 void RAS_StorageVao::BindPrimitives()
 {
-	glBindVertexArray(m_id);
+	GPU_bind_vertex_array(m_id);
 }
 
 void RAS_StorageVao::UnbindPrimitives()
 {
-	glBindVertexArray(0);
+	GPU_bind_vertex_array(0);
 }