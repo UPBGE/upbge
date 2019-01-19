b3b060b3a7f67248ca812ec7e438f1fdcafb78cf
Ulysse Martin <you.le@live.fr>
2019-01-19 01:44:36 +0100

    revert wrong code

--- source/gameengine/Ketsji/BL_Action.cpp
+++ source/gameengine/Ketsji/BL_Action.cpp
@@ -461,19 +461,17 @@ void BL_Action::Update(float curtime, bool applyToObject)
 	if (m_obj->GetGameObjectType() == SCA_IObject::OBJ_ARMATURE)
 	{
 		Object *ob = m_obj->GetBlenderObject(); //eevee
-		if (ob->adt->action == m_action) {
-			DEG_id_tag_update(&ob->id, ID_RECALC_TRANSFORM);
+		DEG_id_tag_update(&ob->id, ID_RECALC_TRANSFORM);
 
-			Scene *sc = scene->GetBlenderScene();
-			ViewLayer *view_layer = BKE_view_layer_default_view(sc);
-			Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
+		Scene *sc = scene->GetBlenderScene();
+		ViewLayer *view_layer = BKE_view_layer_default_view(sc);
+		Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
 
-			BLI_mutex_lock(&object_update_lock);
-			BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 8);
-			BLI_mutex_unlock(&object_update_lock);
+		BLI_mutex_lock(&object_update_lock);
+		BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 8);
+		BLI_mutex_unlock(&object_update_lock);
 
-			scene->ResetTaaSamples();
-		}
+		scene->ResetTaaSamples();
 
 		//// Handle blending between armature actions
 		//if (m_blendin && m_blendframe < m_blendin) {
@@ -514,21 +512,18 @@ void BL_Action::Update(float curtime, bool applyToObject)
 
 		// TEST KEYFRAMED MODIFIERS (WRONG CODE BUT JUST FOR TESTING PURPOSE)
 		if (ob->modifiers.first) {
+			// TODO: We need to find the good notifier per action
+			DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
 
-			if (ob->adt->action == m_action) {
-				// TODO: We need to find the good notifier per action
-				DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
+			Scene *sc = scene->GetBlenderScene();
+			ViewLayer *view_layer = BKE_view_layer_default_view(sc);
+			Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
 
-				Scene *sc = scene->GetBlenderScene();
-				ViewLayer *view_layer = BKE_view_layer_default_view(sc);
-				Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
+			BLI_mutex_lock(&object_update_lock);
+			BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 41);
+			BLI_mutex_unlock(&object_update_lock);
 
-				BLI_mutex_lock(&object_update_lock);
-				BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 41);
-				BLI_mutex_unlock(&object_update_lock);
-
-				scene->ResetTaaSamples();
-			}
+			scene->ResetTaaSamples();
 		}
 		//BL_DeformableGameObject *obj = (BL_DeformableGameObject*)m_obj;
 		//BL_ShapeDeformer *shape_deformer = dynamic_cast<BL_ShapeDeformer*>(obj->GetDeformer());
