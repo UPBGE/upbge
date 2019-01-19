bcf2c1c247eccdcdae894398cdfd4cba0bc60b39
Ulysse Martin <you.le@live.fr>
2019-01-19 07:19:49 +0100

    restrict a bit update modifiers action field
    Still not precise enough but...

--- source/gameengine/Ketsji/BL_Action.cpp
+++ source/gameengine/Ketsji/BL_Action.cpp
@@ -59,6 +59,7 @@ extern "C" {
 #include "BKE_object.h"
 #include "DNA_anim_types.h"
 #include "DNA_material_types.h"
+#include "DNA_modifier_types.h"
 #include "DNA_scene_types.h"
 #include "depsgraph/DEG_depsgraph.h"
 }
@@ -511,19 +512,22 @@ void BL_Action::Update(float curtime, bool applyToObject)
 		Object *ob = m_obj->GetBlenderObject(); //eevee
 
 		// TEST KEYFRAMED MODIFIERS (WRONG CODE BUT JUST FOR TESTING PURPOSE)
-		if (ob->modifiers.first) {
-			// TODO: We need to find the good notifier per action
-			DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
+		for (ModifierData *md = (ModifierData *)ob->modifiers.first; md; md = (ModifierData *)md->next) {
+			if (BKE_object_modifier_use_time(ob, md)) {
+				// TODO: We need to find the good notifier per action
+				DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
 
-			Scene *sc = scene->GetBlenderScene();
-			ViewLayer *view_layer = BKE_view_layer_default_view(sc);
-			Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
+				Scene *sc = scene->GetBlenderScene();
+				ViewLayer *view_layer = BKE_view_layer_default_view(sc);
+				Depsgraph *depsgraph = BKE_scene_get_depsgraph(sc, view_layer, false);
 
-			BLI_mutex_lock(&object_update_lock);
-			BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 41);
-			BLI_mutex_unlock(&object_update_lock);
+				BLI_mutex_lock(&object_update_lock);
+				BKE_object_modifier_update_subframe(depsgraph, sc, ob, true, 5, m_localframe, 41);
+				BLI_mutex_unlock(&object_update_lock);
 
-			scene->ResetTaaSamples();
+				scene->ResetTaaSamples();
+				break;
+			}
 		}
 		//BL_DeformableGameObject *obj = (BL_DeformableGameObject*)m_obj;
 		//BL_ShapeDeformer *shape_deformer = dynamic_cast<BL_ShapeDeformer*>(obj->GetDeformer());
