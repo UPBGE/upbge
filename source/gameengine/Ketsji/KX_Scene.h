4dfa23f3f8df2d338ea8d84decbd031e03a07233
Ulysse Martin <you.le@live.fr>
2020-02-17 08:15:49 +0100

    Fix minor backward compatibility for SetCamera actuator

--- source/gameengine/Ketsji/KX_Scene.h
+++ source/gameengine/Ketsji/KX_Scene.h
@@ -139,6 +139,7 @@ protected:
   Object *m_gameDefaultCamera;
   int m_shadingTypeBackup;
   int m_shadingFlagBackup;
+  int m_rv3dPersBackup;
   std::vector<struct Collection *> m_overlay_collections;
   struct GPUViewport *m_currentGPUViewport;
   /* In the current state of the code, we need this
