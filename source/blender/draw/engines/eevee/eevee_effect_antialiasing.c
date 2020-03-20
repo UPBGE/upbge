fb3b22b179cbd51d817cf0a5c89d0e81778aad17
Ulysse Martin <you.le@live.fr>
2020-03-20 20:43:45 +0100

    use jitter32

--- source/blender/draw/engines/eevee/eevee_effect_antialiasing.c
+++ source/blender/draw/engines/eevee/eevee_effect_antialiasing.c
@@ -257,7 +257,7 @@ bool eevee_antialiasing_setup(EEVEE_Data *vedata)
     const DRWView *default_view = DRW_view_default_get();
     float *transform_offset;
 
-    transform_offset = e_data.jitter_16[min_ii(vedata->stl->effects->taa_current_sample, 16)];
+    transform_offset = e_data.jitter_32[min_ii(vedata->stl->effects->taa_current_sample, 32)];
 
     /* construct new matrices from transform delta */
     float winmat[4][4], viewmat[4][4], persmat[4][4];
