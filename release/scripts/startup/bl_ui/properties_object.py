c8dce7d75ba9ba7c49062ea4671aee8b8671d198
Jorge Bernal <jbernalmartinez@gmail.com>
2020-04-06 07:35:25 +0200

    fix text

--- release/scripts/startup/bl_ui/properties_object.py
+++ release/scripts/startup/bl_ui/properties_object.py
@@ -217,7 +217,7 @@ class OBJECT_PT_collections(ObjectButtonsPanel, Panel):
                 row.prop(collection, "instance_offset", text="")
 
                 row = col.box().row()
-                row.prop(collection, "use_collection_spawn", text="Spawn behaviour when instanced")
+                row.prop(collection, "use_collection_spawn", text="")
 
 
 class OBJECT_PT_display(ObjectButtonsPanel, Panel):
