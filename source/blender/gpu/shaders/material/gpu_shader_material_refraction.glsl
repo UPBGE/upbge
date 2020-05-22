2d5f479b84d57c19f557680e55051e362cc527cf
Ulysse Martin <you.le@live.fr>
2020-05-22 15:45:26 +0200

    recover material refraction

--- source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
@@ -1,3 +1,5 @@
+#ifndef SHADOW_ESM
+
 #ifndef VOLUMETRICS
 void node_bsdf_refraction(vec4 color, float roughness, float ior, vec3 N, out Closure result)
 {
@@ -14,3 +16,24 @@ void node_bsdf_refraction(vec4 color, float roughness, float ior, vec3 N, out Cl
 /* Stub refraction because it is not compatible with volumetrics. */
 #  define node_bsdf_refraction
 #endif
+
+#else // Old Shadows
+
+#ifndef VOLUMETRICS
+void node_bsdf_refraction(vec4 color, float roughness, float ior, vec3 N, out Closure result)
+{
+  N = normalize(N);
+  vec3 out_refr;
+  color.rgb *= (refractionDepth > 0.0) ? color.rgb : vec3(1.0); /* Simulate 2 absorption event. */
+  eevee_closure_refraction(N, roughness, ior, out_refr);
+  vec3 vN = mat3(ViewMatrix) * N;
+  result = CLOSURE_DEFAULT;
+  result.ssr_normal = normal_encode(vN, viewCameraVec);
+  result.radiance = out_refr * color.rgb;
+}
+#else
+/* Stub refraction because it is not compatible with volumetrics. */
+#  define node_bsdf_refraction
+#endif
+
+#endif // Old Shadows
