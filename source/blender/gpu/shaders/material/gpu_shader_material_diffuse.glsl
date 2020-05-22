994f91d80608a090cace992568a3b29c2d47a798
Ulysse Martin <you.le@live.fr>
2020-05-22 15:30:45 +0200

    recover old material diffuse

--- source/blender/gpu/shaders/material/gpu_shader_material_diffuse.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_diffuse.glsl
@@ -1,3 +1,5 @@
+#ifndef SHADOW_ESM
+
 #ifndef VOLUMETRICS
 void node_bsdf_diffuse(vec4 color, float roughness, vec3 N, out Closure result)
 {
@@ -11,3 +13,21 @@ void node_bsdf_diffuse(vec4 color, float roughness, vec3 N, out Closure result)
 /* Stub diffuse because it is not compatible with volumetrics. */
 #  define node_bsdf_diffuse
 #endif
+
+#else // Old Shadows
+
+#ifndef VOLUMETRICS
+void node_bsdf_diffuse(vec4 color, float roughness, vec3 N, out Closure result)
+{
+  N = normalize(N);
+  result = CLOSURE_DEFAULT;
+  eevee_closure_diffuse(N, color.rgb, 1.0, result.radiance);
+  closure_load_ssr_data(vec3(0.0), 0.0, N, viewCameraVec, -1, result);
+  result.radiance *= color.rgb;
+}
+#else
+/* Stub diffuse because it is not compatible with volumetrics. */
+#  define node_bsdf_diffuse
+#endif
+
+#endif // Old Shadows
