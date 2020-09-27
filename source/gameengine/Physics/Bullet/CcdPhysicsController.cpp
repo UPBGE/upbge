92aa6b86c6e81939f1579a31897995e143619f76
Ulysse Martin <you.le@live.fr>
2020-09-27 07:10:22 +0200

    Revert "blah"
    This reverts commit a7996c5d6924cbbfa60f8ee7c2640d3361415fdc.

--- source/gameengine/Physics/Bullet/CcdPhysicsController.cpp
+++ source/gameengine/Physics/Bullet/CcdPhysicsController.cpp
@@ -773,10 +773,10 @@ bool CcdPhysicsController::SynchronizeMotionStates(float time)
     if (sb->m_pose.m_bframe) {
       btVector3 worldPos = sb->m_pose.m_com;
       //btQuaternion worldquat;
-      btMatrix3x3 trs = sb->m_pose.m_rot * sb->m_pose.m_scl;
+      //btMatrix3x3 trs = sb->m_pose.m_rot * sb->m_pose.m_scl;
       //trs.getRotation(worldquat);
       m_MotionState->SetWorldPosition(ToMoto(worldPos));
-      m_MotionState->SetWorldOrientation(ToMoto(trs));
+      //m_MotionState->SetWorldOrientation(ToMoto(worldquat));
     }
     else {
       /*btVector3 aabbMin, aabbMax;
