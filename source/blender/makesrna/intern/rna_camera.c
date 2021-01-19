69a3224cec9ba59f7c3dfdfaff44d81acbce2c12
youle31 <you.le@live.fr>
2021-01-19 06:41:38 +0100

    UPBGE: Fix a compilation issue

--- source/blender/makesrna/intern/rna_camera.c
+++ source/blender/makesrna/intern/rna_camera.c
@@ -630,7 +630,6 @@ void RNA_def_camera(BlenderRNA *brna)
   prop = RNA_def_property(srna, "lod_factor", PROP_FLOAT, PROP_NONE);
   RNA_def_property_float_sdna(prop, NULL, "lodfactor");
   RNA_def_property_range(prop, 0.0f, FLT_MAX);
-  RNA_def_property_float_default(prop, 1.0f);
   RNA_def_property_ui_text(prop, "Level of Detail Distance Factor", "The factor applied to distance computed in Lod");
   RNA_def_property_update(prop, NC_OBJECT | ND_LOD, NULL);
 
