4f0ce103a3d5110c6416d2ca937a7f5e6c20adcb
youle31 <you.le@live.fr>
2021-02-05 15:23:19 +0100

    identation + cleanup in CM_clock.h/.cpp

--- source/gameengine/Common/CM_Clock.cpp
+++ source/gameengine/Common/CM_Clock.cpp
@@ -2,21 +2,21 @@
 
 CM_Clock::CM_Clock()
 {
-	Reset();
+  Reset();
 }
 
 void CM_Clock::Reset()
 {
-	m_start = m_clock.now();
+  m_start = m_clock.now();
 }
 
 double CM_Clock::GetTimeSecond() const
 {
-	return GetTimeNano() * 1e-9;
+  return GetTimeNano() * 1e-9;
 }
 
 CM_Clock::Rep CM_Clock::GetTimeNano() const
 {
-	const std::chrono::high_resolution_clock::time_point now = m_clock.now();
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(now - m_start).count();
+  const std::chrono::high_resolution_clock::time_point now = m_clock.now();
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(now - m_start).count();
 }
--- source/gameengine/Common/CM_Clock.h
+++ source/gameengine/Common/CM_Clock.h
@@ -1,24 +1,21 @@
-#ifndef __CM_CLOCK_H__
-#define __CM_CLOCK_H__
+#pragma once
 
 #include <chrono>
 
 class CM_Clock
 {
 public:
-	using Rep = std::chrono::nanoseconds::rep;
+  using Rep = std::chrono::nanoseconds::rep;
 
 private:
-	std::chrono::high_resolution_clock::time_point m_start;
-	std::chrono::high_resolution_clock m_clock;
+  std::chrono::high_resolution_clock::time_point m_start;
+  std::chrono::high_resolution_clock m_clock;
 
 public:
-	CM_Clock();
+  CM_Clock();
 
-	void Reset();
+  void Reset();
 
-	double GetTimeSecond() const;
-	Rep GetTimeNano() const;
+  double GetTimeSecond() const;
+  Rep GetTimeNano() const;
 };
-
-#endif  // __CM_CLOCK_H__
