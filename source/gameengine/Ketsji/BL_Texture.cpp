00c5d0dc18bbd4da9cfd6fc8790d3f15bd7f646e
youle31 <you.le@live.fr>
2021-04-05 01:26:24 +0200

    UPBGE: Try to fix BL_Texture set bindcode not working
    test file:
    
    https://ufile.io/v45rjq27

--- source/gameengine/Ketsji/BL_Texture.cpp
+++ source/gameengine/Ketsji/BL_Texture.cpp
@@ -105,6 +105,12 @@ BL_Texture::~BL_Texture()
 	copy_v3_v3(m_mtex->size, m_savedData.uvsize);
 
 	if (m_gpuTex) {
+		if (!m_isCubeMap) {
+			GetImage()->bindcode[TEXTARGET_TEXTURE_2D] = m_savedData.bindcode;
+		}
+		else {
+			GetImage()->bindcode[TEXTARGET_TEXTURE_CUBE_MAP] = m_savedData.bindcode;
+		}
 		GPU_texture_set_opengl_bindcode(m_gpuTex, m_savedData.bindcode);
 		GPU_texture_free(m_gpuTex);
 	}
@@ -512,6 +518,12 @@ int BL_Texture::pyattr_set_bind_code(EXP_PyObjectPlus *self_v, const EXP_PYATTRI
 		return PY_SET_ATTR_FAIL;
 	}
 
+	if (!self->m_isCubeMap) {
+		self->GetImage()->bindcode[TEXTARGET_TEXTURE_2D] = val;
+	}
+	else {
+		self->GetImage()->bindcode[TEXTARGET_TEXTURE_CUBE_MAP] = val;
+	}
 	self->m_bindCode = val;
 	return PY_SET_ATTR_SUCCESS;
 }
