16fca963fd34f2bf1dc8c242b541a30a88d18039
youle31 <you.le@live.fr>
2021-04-09 19:09:02 +0200

    16 samples

--- source/blender/draw/engines/eevee/shaders/lights_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/lights_lib.glsl
@@ -172,12 +172,24 @@ vec4 sample_cascade(sampler2DArray tex, vec2 co, float cascade_id)
 #define scube(x) shadows_cube_data[x]
 #define scascade(x) shadows_cascade_data[x]
 
-vec2 poissonDisk[4] = vec2[](
-  vec2( -0.94201624, -0.39906216 ),
-  vec2( 0.94558609, -0.76890725 ),
-  vec2( -0.094184101, -0.92938870 ),
-  vec2( 0.34495938, 0.29387760 )
-);
+vec2 poissonDisk[] = vec2[](    // These are the Poisson Disk Samples
+                                vec2( -0.94201624,  -0.39906216 ),
+                                vec2(  0.94558609,  -0.76890725 ),
+                                vec2( -0.094184101, -0.92938870 ),
+                                vec2(  0.34495938,   0.29387760 ),
+                                vec2( -0.91588581,   0.45771432 ),
+                                vec2( -0.81544232,  -0.87912464 ),
+                                vec2( -0.38277543,   0.27676845 ),
+                                vec2(  0.97484398,   0.75648379 ),
+                                vec2(  0.44323325,  -0.97511554 ),
+                                vec2(  0.53742981,  -0.47373420 ),
+                                vec2( -0.26496911,  -0.41893023 ),
+                                vec2(  0.79197514,   0.19090188 ),
+                                vec2( -0.24188840,   0.99706507 ),
+                                vec2( -0.81409955,   0.91437590 ),
+                                vec2(  0.19984126,   0.78641367 ),
+                                vec2(  0.14383161,  -0.14100790 )
+                               );
 
 float rand_index(vec4 seed4)
 {
@@ -201,9 +213,9 @@ float sample_cube_shadow(int shadow_id, vec3 P)
   float vis = texture(shadowCubeTexture, scoord);
 
   if (sd(shadow_id).use_pcf.x == 1.0) {
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 16; i++) {
       if (texture(shadowCubeTexture, vec4(scoord.xy + poissonDisk[i] / 700.0, scoord.z, scoord.w)) < scoord.w - sd(shadow_id).sh_bias) {
-        int index = int(4.0 * rand_index(vec4(gl_FragCoord.xyy, i))) % 4; // A random number between 0 and 15, different for each pixel (and each i !)
+        int index = int(16.0 * rand_index(vec4(gl_FragCoord.xyy, i))) % 16; // A random number between 0 and 15, different for each pixel (and each i !)
         vis -= 0.2 * (1.0 - texture(shadowCubeTexture, vec4(scoord.xy + poissonDisk[index] / 700.0, (scoord.z-sd(shadow_id).sh_bias)/scoord.w, scoord.w)));
       }
     }
