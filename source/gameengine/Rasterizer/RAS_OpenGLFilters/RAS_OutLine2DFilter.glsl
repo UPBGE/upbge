fb7489c9a529b7a5aab3a8c9c4618985b5462502
EndSSgamesStudio <35436445+EndSSgamesStudio@users.noreply.github.com>
2021-04-11 07:19:36 -0300

    Update RAS_OutLine2DFilter.glsl

--- source/gameengine/Rasterizer/RAS_OpenGLFilters/RAS_OutLine2DFilter.glsl
+++ source/gameengine/Rasterizer/RAS_OpenGLFilters/RAS_OutLine2DFilter.glsl
@@ -2,22 +2,25 @@ uniform sampler2D bgl_RenderedTexture;
 uniform sampler2D bgl_DepthTexture;
 uniform sampler2D bgl_DataTextures[1];
 
-uniform float outlineSize = 0.005;
-uniform vec4 outline_color = vec4(1,0.5,0,1);
+uniform float outlinerSize;
+uniform float outlinerR;
+uniform float outlinerG;
+uniform float outlinerB;
+
 uniform mat2 rot = mat2(vec2(0, -1), vec2(1, 0));
 
 void main(){
 	vec2 coord = gl_TexCoord[0].st;
 	vec4 color = texture2D(bgl_RenderedTexture, coord);
 	vec4 Attach = texture2D(bgl_DataTextures[0], coord);
-	vec2 outl = vec2(outlineSize, 0);
+	vec2 outl = vec2(outlinerSize+0.0015, 0);
 	vec4 AttachOut;
 
 	if (Attach.r == 1){ 
 		for (int i = 0; i < 4; ++i){
 			AttachOut = texture2D(bgl_DataTextures[0], coord + outl);
 			if (AttachOut.r < 1){
-				color = outline_color;
+				color = vec4(outlinerR,outlinerG,outlinerB,1); //vec4(1,0.5,0,1);
 			     break;
 			}
         outl = rot * outl;
