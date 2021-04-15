9b830f4117f0ed26690dcd9830593fa65a323e67
youle31 <you.le@live.fr>
2021-04-15 19:39:05 +0200

    UPBGE: Try to fix texture refcount issue

--- source/gameengine/Ketsji/BL_Texture.cpp
+++ source/gameengine/Ketsji/BL_Texture.cpp
@@ -105,10 +105,10 @@ BL_Texture::~BL_Texture()
 	copy_v3_v3(m_mtex->size, m_savedData.uvsize);
 
 	if (m_gpuTex) {
+		CheckValidTexture();
 		GPU_texture_set_opengl_bindcode(m_gpuTex, m_savedData.bindcode);
 		GPU_texture_free(m_gpuTex);
-
-		CheckValidTexture();
+		m_gpuTex = nullptr;
 		SetBindCode(m_savedData.bindcode);
 	}
 }
