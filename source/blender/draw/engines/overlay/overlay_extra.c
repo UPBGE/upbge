c8ebf1f88f3d11899a92d55f33088b0eed79a7b7
youle31 <you.le@live.fr>
2021-04-29 07:26:12 +0200

    UPBGE: Change rb joint pivot shape to have it always in front
    This shape is using others ogl states

--- source/blender/draw/engines/overlay/overlay_extra.c
+++ source/blender/draw/engines/overlay/overlay_extra.c
@@ -450,7 +450,7 @@ static void OVERLAY_pivot(OVERLAY_ExtraCallBuffers *cb, Object *ob, int theme_id
       scale_m4_fl(tmp, 0.2f);
       copy_v3_v3(tmp[3], xyz);
       mul_m4_m4m4(tmp, ob->obmat, tmp);
-      DRW_buffer_add_entry(cb->empty_sphere, color, tmp);
+      DRW_buffer_add_entry(cb->origin_xform, color, tmp);
     }
   }
 }
