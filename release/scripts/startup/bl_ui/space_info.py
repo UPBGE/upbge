298362fa450dd0743e6fc01c8027409955e45c0a
youle31 <you.le@live.fr>
2021-05-03 20:03:49 +0200

    UPBGE: Make shuvit online API the default in help > Python API reference

--- release/scripts/startup/bl_ui/space_info.py
+++ release/scripts/startup/bl_ui/space_info.py
@@ -361,7 +361,7 @@ class INFO_MT_help(Menu):
 
         layout.operator(
                 "wm.url_open", text="Python API Reference", icon='URL',
-                ).url = "https://pythonapi.upbge.org/"
+                ).url = "https://shuvit.org/python_api/contents.html"
 
         layout.operator("wm.operator_cheat_sheet", icon='TEXT')
         layout.operator("wm.sysinfo", icon='TEXT')
