e57e2e4524c7458382ebd36db41d766e083cf250
youle31 <you.le@live.fr>
2021-08-11 21:29:29 +0200

    UPBGE: Add experimental support for openXR
    To be tested by BluePrintRandom as he has a headset.
    
    Also, it would be nice to find less generic functions to call than
    WM_events_do_notifier and WM_event_do_handler (more specific things
    related to XR)

--- source/gameengine/Ketsji/KX_Scene.cpp
+++ source/gameengine/Ketsji/KX_Scene.cpp
@@ -57,6 +57,8 @@
 #include "GPU_viewport.h"
 #include "WM_api.h"
 #include "wm_draw.h"
+#include "wm_event_system.h"
+#include "xr/wm_xr.h"
 
 #include "BL_BlenderConverter.h"
 #include "BL_BlenderDataConversion.h"
@@ -774,6 +776,16 @@ void KX_Scene::RenderAfterCameraSetup(KX_Camera *cam,
       }
 
       CTX_wm_view3d(C)->camera = cam->GetBlenderObject();
+
+#ifdef WITH_XR_OPENXR
+      wmWindowManager *wm = CTX_wm_manager(C);
+      if (WM_xr_session_exists(&wm->xr)) {
+        if (WM_xr_session_is_ready(&wm->xr)) {
+          wm_event_do_handlers(C); //TODO: Find more specific XR code
+          wm_event_do_notifiers(C); //TODO: Find more specific XR code
+        }
+      }
+#endif
 
       ED_region_tag_redraw(CTX_wm_region(C));
       wm_draw_update(C);
