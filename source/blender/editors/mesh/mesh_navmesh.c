3da8230377d0116a1f39695401ceff731d7857a5
youle31 <you.le@live.fr>
2021-09-11 16:22:56 +0200

    UPBGE: Recover lordloki work #746
    To fix #746

--- source/blender/editors/mesh/mesh_navmesh.c
+++ source/blender/editors/mesh/mesh_navmesh.c
@@ -738,6 +738,7 @@ static int navmesh_clear_exec(bContext *C, wmOperator *UNUSED(op))
   Mesh *me = ob->data;
 
   CustomData_free_layers(&me->pdata, CD_RECAST, me->totpoly);
+  ob->gameflag &= ~OB_NAVMESH;
 
   DEG_id_tag_update(&me->id, ID_RECALC_GEOMETRY);
   WM_event_add_notifier(C, NC_GEOM | ND_DATA, &me->id);
@@ -748,7 +749,7 @@ static int navmesh_clear_exec(bContext *C, wmOperator *UNUSED(op))
 void MESH_OT_navmesh_clear(struct wmOperatorType *ot)
 {
   /* identifiers */
-  ot->name = "NavMesh Clear Data";
+  ot->name = "Remove NavMesh";
   ot->description = "Remove navmesh data from this mesh";
   ot->idname = "MESH_OT_navmesh_clear";
 
