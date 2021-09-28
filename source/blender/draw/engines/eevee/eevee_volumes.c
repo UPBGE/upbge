68034e51e03508dd1d2ddef398184824a4da4788
youle31 <you.le@live.fr>
2021-09-28 14:47:02 +0200

    UPBGE: Missing code in previous commit

--- source/blender/draw/engines/eevee/eevee_volumes.c
+++ source/blender/draw/engines/eevee/eevee_volumes.c
@@ -167,7 +167,7 @@ void EEVEE_volumes_init(EEVEE_ViewLayerData *sldata, EEVEE_Data *vedata)
   }
 
   if (do_taa) {
-    if (!(scene_eval->eevee.flag &
+    if (!(scene_eval->eevee.gameflag &
           SCE_EEVEE_VOLUMETRIC_BLENDING)) { /* UPBGE, Force to use frostbite's
                                                paper solution to avoid ugly banding */
       common_data->vol_history_alpha = 0.0f;
