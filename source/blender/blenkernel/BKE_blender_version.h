b57b1b103f253b30580c8938d5c1f324fd3d7ee4
Jorge Bernal <jbernalmartinez@gmail.com>
2021-11-25 21:19:37 +0100

    UPBGE: Now we are in release candidate stage

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -41,7 +41,7 @@ extern "C" {
 
 #define UPBGE_VERSION_PATCH 0
 /** alpha/beta/rc/release, docs use this. */
-#define UPBGE_VERSION_CYCLE beta
+#define UPBGE_VERSION_CYCLE rc
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
