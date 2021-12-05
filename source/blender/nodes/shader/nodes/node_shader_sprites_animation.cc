ef1e3bfa8f1e65dcbce9999738235d774112cc30
youle31 <you.le@live.fr>
2021-12-05 20:35:14 +0100

    UPBGE: Move sprite node to c++
    See: d5efda72f501ad95679d7ac554086a1fb18c1ac0

--- source/blender/nodes/shader/nodes/node_shader_sprites_animation.cc
+++ source/blender/nodes/shader/nodes/node_shader_sprites_animation.cc
@@ -15,7 +15,7 @@
  *
  */
 
-#include "../node_shader_util.h"
+#include "node_shader_util.hh"
 
 /* **************** SPRITES ANIMATION - UPBGE **************** */
 
@@ -49,8 +49,8 @@ void register_node_type_sh_sprites_animation(void)
   sh_fn_node_type_base(
       &ntype, SH_NODE_SPRITES_ANIMATION, "Sprites Animation", NODE_CLASS_SHADER, 0);
   node_type_socket_templates(&ntype, sh_node_sprites_animation_in, sh_node_sprites_animation_out);
-  node_type_init(&ntype, NULL);
-  node_type_storage(&ntype, "", NULL, NULL);
+  node_type_init(&ntype, nullptr);
+  node_type_storage(&ntype, "", nullptr, nullptr);
   node_type_gpu(&ntype, gpu_shader_sprites_animation);
 
   nodeRegisterType(&ntype);
