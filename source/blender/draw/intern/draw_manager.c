3a90925d1107c41ad958c0f88c63717db8836914
youle31 <you.le@live.fr>
2022-05-24 21:27:48 +0200

    UPBGE: Fix compilation after merge

--- source/blender/draw/intern/draw_manager.c
+++ source/blender/draw/intern/draw_manager.c
@@ -3441,7 +3441,7 @@ void DRW_game_render_loop(bContext *C,
   /* Update UBO's */
   DRW_globals_update();
 
-  DRW_curves_init();
+  DRW_curves_init(DST.vmempool);
   DRW_volume_init(DST.vmempool);
   DRW_smoke_init(DST.vmempool);
 
