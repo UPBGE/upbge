100a3db71db5fc14f223e9525e4fb4c410c4451e
youle31 <you.le@live.fr>
2023-07-05 16:31:03 +0200

    UPBGE: Remove unused versioning code
    not so important i guess

--- source/blender/blenloader/intern/versioning_260.cc
+++ source/blender/blenloader/intern/versioning_260.cc
@@ -426,25 +426,6 @@ static void do_versions_nodetree_frame_2_64_6(bNodeTree *ntree)
   }
 }
 
-static void do_version_logic_264(ListBase *regionbase)
-{
-  ARegion *ar;
-
-  /* view settings for logic changed */
-  for (ar = regionbase->first; ar; ar = ar->next) {
-    if (ar->regiontype == RGN_TYPE_WINDOW) {
-      if (ar->v2d.keeptot == 0) {
-        ar->v2d.maxzoom = 1.5f;
-
-        ar->v2d.keepzoom = V2D_KEEPZOOM | V2D_LIMITZOOM | V2D_KEEPASPECT;
-        ar->v2d.keeptot = V2D_KEEPTOT_BOUNDS;
-        ar->v2d.align = V2D_ALIGN_NO_POS_Y | V2D_ALIGN_NO_NEG_X;
-        ar->v2d.keepofs = V2D_KEEPOFS_Y;
-      }
-    }
-  }
-}
-
 static void do_versions_affine_tracker_track(MovieTrackingTrack *track)
 {
   for (int i = 0; i < track->markersnr; i++) {
