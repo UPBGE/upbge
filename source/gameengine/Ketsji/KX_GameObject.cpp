cde77da9c1cae450209b9f6c8552562006666cb0
youle31 <you.le@live.fr>
2023-07-14 16:09:07 +0200

    UPBGE: Complete dc5370d8c7347abd68f9b847

--- source/gameengine/Ketsji/KX_GameObject.cpp
+++ source/gameengine/Ketsji/KX_GameObject.cpp
@@ -382,7 +382,14 @@ void KX_GameObject::ReplicateBlenderObject()
      * See: https://github.com/UPBGE/upbge/issues/1818 - Would maybe need more investigations
      * for a better fix... */
     if (GetScene()->GetOverlayCamera()) {
-      GetScene()->AppendToIdsToUpdateInOverlayPass(&scene->id, ID_RECALC_EDITORS);
+      View3D *v3d = CTX_wm_view3d(C);
+      bool is_realtime_compositor_enabled = scene->use_nodes && scene->nodetree &&
+                                            v3d->shading.use_compositor !=
+                                                V3D_SHADING_USE_COMPOSITOR_DISABLED &&
+                                            v3d->shading.type >= OB_MATERIAL;
+      if (is_realtime_compositor_enabled) {
+        GetScene()->AppendToIdsToUpdateInOverlayPass(&scene->id, ID_RECALC_EDITORS);
+      }
     }
 
     if (ob->parent) {
