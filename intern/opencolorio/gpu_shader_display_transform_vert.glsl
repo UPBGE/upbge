50687f6f6f41ca201955b04a39abd4e700cc5541
Nightlygine <168599190+Nightlygine@users.noreply.github.com>
2024-05-07 18:11:57 -0300

    Delete intern directory
    Signed-off-by: Nightlygine <168599190+Nightlygine@users.noreply.github.com>

--- intern/opencolorio/gpu_shader_display_transform_vert.glsl
+++
@@ -1,9 +0,0 @@
-/* SPDX-FileCopyrightText: 2016-2022 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-
-void main()
-{
-  gl_Position = ModelViewProjectionMatrix * vec4(pos.xy, 0.0f, 1.0f);
-  texCoord_interp = texCoord;
-}
