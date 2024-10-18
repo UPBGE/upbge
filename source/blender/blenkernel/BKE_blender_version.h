cd7d572df407fcaa8bc85354beaf29aa996c1127
John Kiril Swenson <kirilswenson@gmail.com>
2024-10-18 05:50:09 +0200

    VSE: Switch to box select default for timeline
    PR #128051 made some improvements to code by cleaning up properties and
    simplifying logic.
    
    However, the default tool in most spaces is box select. By switching to
    box select default, there should be more front-facing consistency across
    spaces while still retaining near-identical behavior.
    
    This patch would fix the small bug listed in #128671 while keeping the
    simplified code benefits from #128051.
    
    Also:
    - Move selection keymap items up in the Sequencer (Global) keymap so it
      is more visible (similar to Node Editor ordering).
    - Split `side_of_frame` property for ctrl press keymap item into a
      separate keymap item on ctrl click for both LCS and RCS to avoid
      clashing with ctrl+drag for box selects in RCS.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/129028
    
    (cherry picked from commit d5d81c5078047ba3f01ed1b0091c054b8eef91a2)

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -38,7 +38,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 30
+#define BLENDER_FILE_SUBVERSION 31
 
 /* UPBGE file format version. */
 #define UPBGE_FILE_VERSION UPBGE_VERSION
