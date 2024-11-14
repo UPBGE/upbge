zyxcambridge/blender_llm

Unix: use getpwuid() to access home when the variable/tilde expand fails
This was already done in GHOST, but not BKE_appdir_folder_home.

Also null check the return value from getpwuid() as it's not
guaranteed to be non-null.

intern/ghost/intern/GHOST_SystemPathsUnix.cc
@@ -75,9 +75,17 @@ const char *GHOST_SystemPathsUnix::getUserDir(int version, const char *versionst
     else {
       home = getenv("HOME");
       if (home == nullptr) {
-        home = getpwuid(getuid())->pw_dir;
+        if (const passwd *pwuser = getpwuid(getuid())) {
+          home = pwuser->pw_dir;
+        }
       }
-      user_path = string(home) + "/.config/blender/" + versionstr;
+
+      if (home) {
+        user_path = string(home) + "/.config/blender/" + versionstr;
+      }
+      else {
+        return nullptr;
+      }
     }
   }
 

UPBGE/upbge

(cherry picked from commit 6039cb17e63e740bc877498467b06175978b168a)

intern/ghost/intern/GHOST_SystemPathsUnix.cc
@@ -75,9 +75,17 @@ const char *GHOST_SystemPathsUnix::getUserDir(int version, const char *versionst
     else {
       home = getenv("HOME");
       if (home == nullptr) {
-        home = getpwuid(getuid())->pw_dir;
+        if (const passwd *pwuser = getpwuid(getuid())) {
+          home = pwuser->pw_dir;
+        }
       }
-      user_path = string(home) + "/.config/upbge/" + versionstr;
+
+      if (home) {
+        user_path = string(home) + "/.config/upbge/" + versionstr;
+      }
+      else {
+        return nullptr;
+      }
     }
   }
 