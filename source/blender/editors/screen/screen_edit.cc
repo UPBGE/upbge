zyxcambridge/blender_llm

Fix #130583: Crash opening a file when asset shelf is set for non-paint modes
The region type isn't set yet when polling regions just after file read. As a
result, the polling state cannot be evaluated correctly, and `init()` may end
up being called without the previous required call to `on_poll_success()`.

Ensure the region type is set earlier, right at the beginning of screen
initialization after file read. This way all further area/region
referesh/re-init (confusingly called "init") logic can assume it's set.

Pull Request: https://projects.blender.org/blender/blender/pulls/131050

source/blender/editors/screen/screen_edit.cc
@@ -673,7 +673,11 @@ static bool region_poll(const bContext *C,
                         const ScrArea *area,
                         const ARegion *region)
 {
-  if (!region->runtime->type || !region->runtime->type->poll) {
+  if (!region->runtime->type) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  if (!region->runtime->type->poll) {
     /* Show region by default. */
     return true;
   }
@@ -799,6 +803,14 @@ void ED_screens_init(bContext *C, Main *bmain, wmWindowManager *wm)
     if (BKE_workspace_active_get(win->workspace_hook) == nullptr) {
       BKE_workspace_active_set(win->workspace_hook,
                                static_cast<WorkSpace *>(bmain->workspaces.first));
+    }
+
+    const bScreen *screen = WM_window_get_active_screen(win);
+    ED_screen_areas_iter (win, screen, area) {
+      /* Set area and region types early so areas and regions are in a usable state. This may be
+       * needed by further (re-)initialization logic, specifically region polling needs it early on
+       * (see #130583). */
+      ED_area_and_region_types_init(area);
     }
 
     ED_screen_refresh(C, wm, win);

UPBGE/upbge

(cherry picked from commit d48d3b9f7ac39f60e4ce9b12ff05c357bd18f6c8)

source/blender/editors/screen/screen_edit.cc
@@ -671,7 +671,11 @@ static bool region_poll(const bContext *C,
                         const ScrArea *area,
                         const ARegion *region)
 {
-  if (!region->type || !region->type->poll) {
+  if (!region->type) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  if (!region->type->poll) {
     /* Show region by default. */
     return true;
   }
@@ -797,6 +801,14 @@ void ED_screens_init(bContext *C, Main *bmain, wmWindowManager *wm)
     if (BKE_workspace_active_get(win->workspace_hook) == nullptr) {
       BKE_workspace_active_set(win->workspace_hook,
                                static_cast<WorkSpace *>(bmain->workspaces.first));
+    }
+
+    const bScreen *screen = WM_window_get_active_screen(win);
+    ED_screen_areas_iter (win, screen, area) {
+      /* Set area and region types early so areas and regions are in a usable state. This may be
+       * needed by further (re-)initialization logic, specifically region polling needs it early on
+       * (see #130583). */
+      ED_area_and_region_types_init(area);
     }
 
     ED_screen_refresh(C, wm, win);