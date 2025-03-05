09fda47d77a93f3d9eef7ab342608c392e59523d
Jonas Holzman <jonas@holzman.fr>
2025-03-05 12:41:32 +0100

    Fix #134260: Color Picker components partially resetting when pressing Backspace
    Previously, when pressing the Backspace key inside an open color picker,
    the HSV and Hex field components would not refresh. Only the RGB
    components would.
    
    Fixed by calling the attached button function callbacks which propagates
    the color changes to the other color picker buttons from the RGB values.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/135131
    
    (cherry picked from commit 32f8863b0c1ab195f7203bd0ce644f8348d527de)

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -7183,6 +7183,7 @@ static int ui_do_but_HSVCUBE(
 
           ui_color_picker_to_rgb_HSVCUBE_v(hsv_but, def_hsv, rgb);
           ui_but_v3_set(but, rgb);
+          ui_apply_but_func(C, but);
 
           RNA_property_update(C, &but->rnapoin, but->rnaprop);
           return WM_UI_HANDLER_BREAK;
@@ -7453,6 +7454,7 @@ static int ui_do_but_HSVCIRCLE(
 
         hsv_to_rgb_v(def_hsv, rgb);
         ui_but_v3_set(but, rgb);
+        ui_apply_but_func(C, but);
 
         RNA_property_update(C, &but->rnapoin, but->rnaprop);
 
