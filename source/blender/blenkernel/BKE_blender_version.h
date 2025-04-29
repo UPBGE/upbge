b2f01e3ae31e327a7bbc2a2f78f05425d9063923
Thomas Dinges <thomas@blender.org>
2025-04-29 16:09:45 +0200

    Release: 4.4.3
    (cherry picked from commit 802179c51ccc68b776e7a2fad23503b961577013)

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -26,7 +26,7 @@ extern "C" {
 #define UPBGE_VERSION 44
 
 /** Blender patch version for bug-fix releases. */
-#define BLENDER_VERSION_PATCH 2
+#define BLENDER_VERSION_PATCH 3
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE release
 /** Blender release type suffix. LTS or blank. */
