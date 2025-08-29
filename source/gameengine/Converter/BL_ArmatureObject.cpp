153a6bc33ef91fdd74c352ad8bb0ac9a73f0fc19
youle31 <you.le@live.fr>
2025-08-29 07:37:34 +0200

    UPBGE: Cancel something in previous commit

--- source/gameengine/Converter/BL_ArmatureObject.cpp
+++ source/gameengine/Converter/BL_ArmatureObject.cpp
@@ -643,7 +643,7 @@ void BL_ArmatureObject::SetPoseByAction(bAction *action, AnimationEvalContext *e
   // 1. apply action to armature
   PointerRNA ptrrna = RNA_id_pointer_create(&m_objArma->id);
   const blender::animrig::slot_handle_t slot_handle = blender::animrig::first_slot_handle(*action);
-  animsys_evaluate_action(&ptrrna, action, slot_handle, evalCtx, true);
+  animsys_evaluate_action(&ptrrna, action, slot_handle, evalCtx, false);
 
   // 2. update pose
   if (!m_suspendPose) {
