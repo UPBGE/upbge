8f82e3aad070330e2d724166ebad93a290095acb
Thomas Dinges <thomas@blender.org>
2025-11-19 09:01:03 +0100

    Release: Bump to 5.0.1 RC
    (cherry picked from commit e6a681923b11124dc0eb2d3096a174acbe1d8dfd)

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -22,9 +22,9 @@
 #define UPBGE_VERSION 50
 
 /** Blender patch version for bug-fix releases. */
-#define BLENDER_VERSION_PATCH 0
+#define BLENDER_VERSION_PATCH 1
 /** Blender release cycle stage: alpha/beta/rc/release. */
-#define BLENDER_VERSION_CYCLE release
+#define BLENDER_VERSION_CYCLE rc
 /** Blender release type suffix. LTS or blank. */
 #define BLENDER_VERSION_SUFFIX
 
