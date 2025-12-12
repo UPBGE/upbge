b82e43cb193e79339e5f207604ed79896603f585
youle31 <you.le@live.fr>
2025-12-13 00:41:35 +0100

    UPBGE: Prevent crash with nullptr gpuviewport + remove old code
    If no frame is rendered, gpuviewport can be nullptr.
    
    Note: With old code, there was always a render loop executed in KX_Scene
    constructor, then m_currentGPUViewport was always filled (this render
    loop was here to make it possible to initialize BL_Texture in the old
    code). Now it's not needed anymore then this code in KX_Scene
    constructor is removed

--- source/gameengine/Ketsji/KX_Scene.cpp
+++ source/gameengine/Ketsji/KX_Scene.cpp
@@ -312,17 +312,6 @@ KX_Scene::KX_Scene(SCA_IInputDevice *inputDevice,
     /* We want to indicate that we are in bge runtime. The flag can be used in draw code but in
      * depsgraph code too later */
     scene->flag |= SCE_INTERACTIVE;
-
-    bool is_vulkan_backend = GPU_backend_get_type() == GPU_BACKEND_VULKAN;
-    if (!is_vulkan_backend) {
-      /* We call Render here in KX_Scene constructor because
-       * 1: It creates a depsgraph and ensure it will be activated.
-       * 2: We need to create an eevee's cache to initialize
-       * KX_BlenderMaterials and BL_Textures.
-       */
-      const RAS_Rect &viewport = KX_GetActiveEngine()->GetCanvas()->GetViewportArea();
-      RenderAfterCameraSetup(nullptr, nullptr, viewport, false, true);
-    }
   }
   else {
     scene->flag |= SCE_INTERACTIVE_VIEWPORT;
@@ -361,19 +350,23 @@ KX_Scene::~KX_Scene()
 
   if (!KX_GetActiveEngine()->UseViewportRender()) {
     if (!m_isPythonMainLoop) {
-      /* This will free m_currentGPUViewport */
-      GPU_viewport_free(m_currentGPUViewport);
+      if (m_currentGPUViewport) {
+        /* This will free m_currentGPUViewport */
+        GPU_viewport_free(m_currentGPUViewport);
+      }
     }
     else {
       /* If we are in python loop and we called render code */
-      if (!m_initMaterialsGPUViewport) {
+      if (!m_initMaterialsGPUViewport && m_currentGPUViewport) {
         GPU_viewport_free(m_currentGPUViewport);
       }
       else {
         /* It has not been freed before because the main Render loop
          * is not executed then we free it now.
          */
-        GPU_viewport_free(m_initMaterialsGPUViewport);
+        if (m_initMaterialsGPUViewport) {
+          GPU_viewport_free(m_initMaterialsGPUViewport);
+        }
         //DRW_game_python_loop_end(DEG_get_evaluated_view_layer(depsgraph));
       }
     }
