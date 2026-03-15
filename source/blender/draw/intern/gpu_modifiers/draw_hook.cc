036157c8616715e3de5cb7fa86f3e860367725cc
youle31 <you.le@live.fr>
2026-03-15 14:12:42 +0100

    UPBGE: Fix some asserts

--- source/blender/draw/intern/gpu_modifiers/draw_hook.cc
+++ source/blender/draw/intern/gpu_modifiers/draw_hook.cc
@@ -568,9 +568,7 @@ gpu::StorageBuf *HookManager::dispatch_deform(const HookModifierData *hmd,
   }
 
   /* Bind and dispatch */
-  const gpu::shader::SpecializationConstants *constants =
-      &GPU_shader_get_default_constant_state(shader);
-  GPU_shader_bind(shader, constants);
+  GPU_shader_bind(shader);
 
   GPU_storagebuf_bind(ssbo_out, 0);
   GPU_storagebuf_bind(ssbo_in, 1);
@@ -598,7 +596,7 @@ gpu::StorageBuf *HookManager::dispatch_deform(const HookModifierData *hmd,
 
   const int group_size = 256;
   const int num_groups = (msd.verts_num + group_size - 1) / group_size;
-  GPU_compute_dispatch(shader, num_groups, 1, 1, constants);
+  GPU_compute_dispatch(shader, num_groups, 1, 1);
 
   GPU_memory_barrier(GPU_BARRIER_SHADER_STORAGE);
   GPU_shader_unbind();
